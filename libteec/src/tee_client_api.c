17d1addc465a667f375837cdbe4fa7ebac08539b
Kris <krzysiek@leeds.pl>
2016-06-05 23:17:58 +0200

    Check flags before allocating shared memory.
    In TEEC_AllocateSharedMemory check if TEEC_MEM_INPUT, TEEC_MEM_OUTPUT
    or both values are set in shm->flags before allocating shared memory.
    
    Signed-off-by: Krzysztof Kwiatkowski <kris@amongbytes.com>
    Reviewed-by: Joakim Bech <joakim.bech@linaro.org>
    Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
    Tested-by: Joakim Bech <joakim.bech@linaro.org> (QEMU)

--- libteec/src/tee_client_api.c
+++ libteec/src/tee_client_api.c
@@ -605,6 +605,9 @@ TEEC_Result TEEC_RegisterSharedMemory(TEEC_Context *ctx, TEEC_SharedMemory *shm)
 	if (!ctx || !shm)
 		return TEEC_ERROR_BAD_PARAMETERS;
 
+	if (!shm->flags || (shm->flags & ~(TEEC_MEM_INPUT | TEEC_MEM_OUTPUT)))
+		return TEEC_ERROR_BAD_PARAMETERS;
+
 	s = shm->size;
 	if (!s)
 		s = 8;
@@ -631,6 +634,9 @@ TEEC_Result TEEC_AllocateSharedMemory(TEEC_Context *ctx, TEEC_SharedMemory *shm)
 	size_t s;
 
 	if (!ctx || !shm)
+		return TEEC_ERROR_BAD_PARAMETERS;
+
+	if (!shm->flags || (shm->flags & ~(TEEC_MEM_INPUT | TEEC_MEM_OUTPUT)))
 		return TEEC_ERROR_BAD_PARAMETERS;
 
 	s = shm->size;
