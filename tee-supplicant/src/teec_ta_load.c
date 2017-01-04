OP-TEE/optee_client

commit ce5fefe6cba5df9b4f31399b1155ba768eee0b78

Format UUID strings correctly
Prior to this patch although GlobalPlatform specifies that TAs and such
are identified with UUIDs, we don't format them quite right when turning
them into strings. Per https://www.ietf.org/rfc/rfc4122.txt, there
should be another hyphen after the first two bytes of clockSeqAndNode.

Unfortunately, fixing this breaks compatibility between how TAs are
built, and when the OS loads them.

With this patch UUID string are formated with the additional hyphen as
for instance: f81d4fae-7dec-11d0-a765-00a0c91e6bf6

Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
Fixes: https://github.com/OP-TEE/optee_os/issues/857
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

tee-supplicant/src/teec_ta_load.c
@@ -95,7 +95,7 @@ static int try_load_secure_module(const char* prefix,
 	}
 
 	n = snprintf(fname, PATH_MAX,
-		     "%s/%s/%08x-%04x-%04x-%02x%02x%02x%02x%02x%02x%02x%02x.ta",
+		     "%s/%s/%08x-%04x-%04x-%02x%02x-%02x%02x%02x%02x%02x%02x.ta",
 		     prefix, dev_path,
 		     destination->timeLow,
 		     destination->timeMid,

commit 365657667f8968f40237480169fea44fa3fb9949

Add backward compatibility with older TA UUID format
Fixes: ce5fefe6cba ("Format UUID strings correctly")
Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

tee-supplicant/src/teec_ta_load.c
@@ -26,6 +26,7 @@
  */
 #include <dirent.h>
 #include <fcntl.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
@@ -86,22 +87,31 @@ static int try_load_secure_module(const char* prefix,
 {
 	char fname[PATH_MAX];
 	FILE *file = NULL;
-	int n;
+	bool first_try = true;
 	size_t s;
+	int n;
 
 	if (!ta_size || !destination) {
 		printf("wrong inparameter to TEECI_LoadSecureModule\n");
 		return TA_BINARY_NOT_FOUND;
 	}
 
+	/*
+	 * We expect the TA binary to be named after the UUID as per RFC4122,
+	 * that is: xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx.ta
+	 * If the file cannot be open, try the deprecated format:
+	 * xxxxxxxx-xxxx-xxxx-xxxxxxxxxxxxxxxx.ta
+	 */
+again:
 	n = snprintf(fname, PATH_MAX,
-		     "%s/%s/%08x-%04x-%04x-%02x%02x-%02x%02x%02x%02x%02x%02x.ta",
+		     "%s/%s/%08x-%04x-%04x-%02x%02x%s%02x%02x%02x%02x%02x%02x.ta",
 		     prefix, dev_path,
 		     destination->timeLow,
 		     destination->timeMid,
 		     destination->timeHiAndVersion,
 		     destination->clockSeqAndNode[0],
 		     destination->clockSeqAndNode[1],
+		     first_try ? "-" : "",
 		     destination->clockSeqAndNode[2],
 		     destination->clockSeqAndNode[3],
 		     destination->clockSeqAndNode[4],
@@ -119,6 +129,10 @@ static int try_load_secure_module(const char* prefix,
 	file = fopen(fname, "r");
 	if (file == NULL) {
 		DMSG("failed to open the ta %s TA-file", fname);
+		if (first_try) {
+			first_try = false;
+			goto again;
+		}
 		return TA_BINARY_NOT_FOUND;
 	}
 