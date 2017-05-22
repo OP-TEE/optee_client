OP-TEE/optee_client

commit 9faeb1b6b02322c9fce2a0fd1a28abf2ac7ffb5a

tee_client_api: Correct max shared memory size define
Signed-off-by: Lukasz Kostyra <l.kostyra@samsung.com>
Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

public/tee_client_api.h
@@ -40,10 +40,11 @@
 
 /**
  * Defines the maximum size of a single shared memory block, in bytes, of both
- * API allocated and API registered memory. The size is currently set to
- * 512 * kB (512 * 1024).
+ * API allocated and API registered memory. There is no good value to put here
+ * (limits depend on specific config used), so this define does not provide any
+ * restriction in this implementation.
  */
-#define TEEC_CONFIG_SHAREDMEM_MAX_SIZE 0x8000
+#define TEEC_CONFIG_SHAREDMEM_MAX_SIZE ULONG_MAX
 
 /**
  * Flag constants indicating the type of parameters encoded inside the

commit a47e33440e4d3917a03f035f3b71f37dcf383952

client api: fix ULONG_MAX dependency
ULONG_MAX depends on limits.h.

Fixes: 9faeb1b6b023 ("tee_client_api: Correct max shared memory size define")
Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

public/tee_client_api.h
@@ -31,6 +31,7 @@
 
 #include <stdint.h>
 #include <stddef.h>
+#include <limits.h>
 
 /*
  * Defines the number of available memory references in an open session or