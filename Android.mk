ff2eb6162cfbf586b18f65933c40b6f0624d039d
Jerome Forissier <jerome.forissier@linaro.org>
2017-05-30 10:22:48 +0200

    Remove leftovers of SQL FS
    Fixes: 403c9a175794 ("Remove SQL-FS")
    Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

--- Android.mk
+++ Android.mk
@@ -38,44 +38,5 @@ LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/public
 
 include $(BUILD_SHARED_LIBRARY)
 
-ifeq ($(CFG_SQL_FS),y)
-
-#
-# Build libsqlite3.a
-#
-include $(CLEAR_VARS)
-LOCAL_MODULE := libsqlite3
-LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := libsqlite3/src/sqlite3.c
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/libsqlite3/include
-libsqlite3_CFLAGS_REMOVE := -Wall -Wbad-function-cast -Wswitch-default \
-                            -Wfloat-equal -Werror -Wwrite-strings -Wcast-align
-LOCAL_CFLAGS += $(filter-out $(libsqlite3_CFLAGS_REMOVE),$(optee_CFLAGS))
-include $(BUILD_STATIC_LIBRARY)
-
-#
-# Build libsqlfs.so
-#
-include $(CLEAR_VARS)
-LOCAL_MODULE := libsqlfs
-LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := libsqlfs/src/sqlfs.c
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/libsqlfs/include \
-                    $(LOCAL_PATH)/libsqlite3/include
-libsqlfs_CFLAGS_REMOVE := -Wall -Wunused-parameter -Wmissing-prototypes \
-                          -Wdiscarded-qualifiers \
-                          -Wmissing-declarations \
-                          -Wwrite-strings -Wstrict-prototypes
-LOCAL_CFLAGS += $(filter-out $(libsqlfs_CFLAGS_REMOVE),$(optee_CFLAGS)) \
-                -Wno-missing-format-attribute
-LOCAL_STATIC_LIBRARIES := libsqlite3
-LOCAL_LDLIBS := -llog
-# Note: building a shared library (.so) here, because statically
-# linking a LGPL library has licensing implications. Make sure you review
-# and comply with libsqlfs/COPYING section 6 before doing so.
-include $(BUILD_SHARED_LIBRARY)
-
-endif # CFG_SQL_FS == y
-
 # TEE Supplicant
 include $(LOCAL_PATH)/tee-supplicant/tee_supplicant_android.mk
