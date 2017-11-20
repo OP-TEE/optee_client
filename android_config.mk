d45a1f6507640cfd23db0ea0d38e7c850e2a4199
Etienne Carriere <etienne.carriere@linaro.org>
2017-11-20 16:40:05 +0100

    autotools for optee_client
    Use the same layout for autotools and Android: rely on the same
    directives and handle directives from both sources and make scripts.
    
    Main changes:
    - Config variables and dependencies are defined in configure.ac
    - Make scripts move from Makefile for Makefile.am
    - add optee_client.pc.in to help packaging support from autotools.
    - config.mk is only for Android => renamed to Android.
    - flags.mk no more used => removed.
    - Remove the deprecated make targets checkpatch-xxx from main make script.
    
    So what is new now?
    - CFG_xxx directives that affect optee_client are now clearly listed
      (see configure.ac) with a nice help info.
    - With new Makefile.am format, adding a CFG_xxx requires to define it in
      configure.ac and handle it from Makefile.am.
    - The libtool builds the library in shared and archive forms.
    - Generic installation root path is set from 'configure --prefix=PATH'.
         PATH/lib/libLIBNAME.so[-x.y.z]
         PATH/lib/libLIBNAME.a
         PATH/bin/tee_supplicant
    - autotools generates generic make targets (all, [un]install *-*clean,
      cscope, etc)
    
    Libteec is deliver as libteec.so.0.1.0.
    
    How to use this compared to previous?
    Old scheme:
     get:   # git clone .../optee_client.git
     build: # make -C .../optee_client [CFG_...]
     Note: target all includes target install.
    
    New scheme:
     get:   # git clone .../optee_client.git
            # cd .../optee_client && autoreconf -i
         or # cd .../optee_client && ./bootstrap
     build: # cd .../optee_client
            # ./configure [CFG_...]
            # make && make install
     Note: need to run configure to change the config.
    
    Unknown issue:
    - not tried yet the pkgconfig part
    - update README
    
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

--- android_config.mk
+++ android_config.mk
@@ -1,23 +1,13 @@
-#########################################################################
-# Public variables                                                      #
-# Developers may override these values when calling the makefile,       #
-# as for example                                                        #
-#       CFG_TEE_CLIENT_LOG_LEVEL=1 make                                 #
-# or by declaring the variable in their environement, as for example    #
-#       export CFG_TEE_CLIENT_LOG_LEVEL=1                               #
-#       make                                                            #
-#########################################################################
-
 # CFG_TEE_CLIENT_LOG_LEVEL
 #   Client (User Non Secure) log level
 #   Supported values: 0 (no traces) to 4 (all traces)
-CFG_TEE_CLIENT_LOG_LEVEL?=1
+CFG_TEE_CLIENT_LOG_LEVEL ?= 1
 export CFG_TEE_CLIENT_LOG_LEVEL
 
 # CFG_TEE_SUPP_LOG_LEVEL
 #   Supplicant log level
 #   Supported values: 0 (no traces) to 4 (all traces)
-CFG_TEE_SUPP_LOG_LEVEL?=1
+CFG_TEE_SUPP_LOG_LEVEL ?= 1
 export CFG_TEE_SUPP_LOG_LEVEL
 
 # CFG_TEE_CLIENT_LOG_FILE
@@ -30,7 +20,7 @@ CFG_TEE_SUPP_LOG_FILE ?= \"/data/teesupp.log\"
 
 # CFG_TEE_CLIENT_LOAD_PATH
 # The location of the client library file.
-CFG_TEE_CLIENT_LOAD_PATH ?= /lib
+CFG_TEE_CLIENT_LOAD_PATH ?= /system/lib
 
 # CFG_TEE_FS_PARENT_PATH
 # Path to folder that will contain TEE filesystem.
@@ -53,18 +43,3 @@ O               ?= out
 
 # To be used instead of $(O) in sub-directories
 OO := $(if $(filter /%,$(O)),$(O),$(CURDIR)/../$(O))
-
-#########################################################################
-# Private Values                                                        #
-#########################################################################
-
-# Check that settings are coherent.
-
-ifdef ARM_TOOLCHAIN_DIR
-ifeq ($(wildcard ${ARM_TOOLCHAIN_DIR}/bin/${ARM_GCC_PREFIX}-gcc),)
-  $(error "ARM_TOOLCHAIN_DIR wrongly setup. Is ${ARM_TOOLCHAIN_DIR}")
-endif
-export ARM_TOOLCHAIN_DIR
-export ARM_GCC_PREFIX
-endif
-
