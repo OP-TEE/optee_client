ab57c5d6d99bd451c71934540c957ab0749fa53f
Etienne Carriere <etienne.carriere@linaro.org>
2018-06-15 12:23:40 +0200

    [review] libsks: fully stubbed cryptoki API
    fix notice
    
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

--- libsks/src/pkcs11_api.c
+++ libsks/src/pkcs11_api.c
@@ -1,7 +1,6 @@
+// SPDX-License-Identifier: BSD-2-Clause
 /*
- * Copyright (c) 2017, Linaro Limited
- *
- * SPDX-License-Identifier: BSD-2-Clause
+ * Copyright (c) 2017-2018, Linaro Limited
  */
 
 #include <pkcs11.h>
