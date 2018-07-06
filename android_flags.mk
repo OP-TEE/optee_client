cb20bb06920488ed7830d0a045ffa248a262abae
Jerome Forissier <jerome.forissier@linaro.org>
2018-07-06 10:32:53 +0200

    Do not set -Werror by default
    Do not use -Werror unless explicitly asked via "make CFG_WERROR=y".
    
    Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
    Reviewed-by: Joakim Bech <joakim.bech@linaro.org>

--- android_flags.mk
+++ android_flags.mk
@@ -9,7 +9,10 @@ CFLAGS          := -Wall -Wbad-function-cast -Wcast-align \
 		   -Wmissing-noreturn -Wmissing-prototypes -Wnested-externs \
 		   -Wpointer-arith -Wshadow -Wstrict-prototypes \
 		   -Wswitch-default \
-		   -Wwrite-strings -Werror
+		   -Wwrite-strings
+ifeq ($(CFG_WERROR),y)
+CFLAGS		+= -Werror
+endif
 CFLAGS          += -c -fPIC
 
 DEBUG       ?= 0
