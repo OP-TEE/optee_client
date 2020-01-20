cbc16cabfe56004b1e8cc59b227dad3d71c5c2a2
Etienne Carriere <etienne.carriere@linaro.org>
2020-01-20 16:33:07 +0100

    libckteec: helpers for Cryptoki/PKCS11 TA IDs conversion
    The PKCS11 TA uses IDs that mostly relate to defined PKCS#11 IDs
    but with different numerical value. These helpers convert PKCS#11
    IDs into/from PKCS11 TA IDs.
    
    This change introduces conversion from GPD TEE Client error codes
    into CryptoKi return values.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
    Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

---
+++ libckteec/src/ck_helpers.c
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (c) 2020, Linaro Limited
+ */
+
+#include "ck_helpers.h"
+
+CK_RV teec2ck_rv(TEEC_Result res)
+{
+	switch (res) {
+	case TEEC_SUCCESS:
+		return CKR_OK;
+	case TEEC_ERROR_OUT_OF_MEMORY:
+		return CKR_DEVICE_MEMORY;
+	case TEEC_ERROR_BAD_PARAMETERS:
+		return CKR_ARGUMENTS_BAD;
+	case TEEC_ERROR_SHORT_BUFFER:
+		return CKR_BUFFER_TOO_SMALL;
+	default:
+		return CKR_FUNCTION_FAILED;
+	}
+}
