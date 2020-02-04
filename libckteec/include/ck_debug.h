c0d19af1889bc3f359fe8c5b90b3699ff0b8ed12
Etienne Carriere <etienne.carriere@linaro.org>
2020-02-04 17:17:56 +0100

    libckteec: debug string helpers for slot/token flags and mechanism info
    Define debug string helpers for slot flag info, token flag info
    and mechanism info.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

--- libckteec/include/ck_debug.h
+++ libckteec/include/ck_debug.h
@@ -11,4 +11,16 @@
 /* Return a pointer to a string buffer of "CKR_xxx\0" return value ID */
 const char *ckr2str(CK_RV id);
 
+/* ckm2str - Return string buffer of "CKM_xxx\0" for a mechanism ID */
+const char *ckm2str(CK_MECHANISM_TYPE id);
+
+/* slot_ckf2str - Return string buffer of "CKF_xxx\0" for a slot flag */
+const char *slot_ckf2str(CK_ULONG flag);
+
+/* token_ckf2str - Return string buffer "CKF_xxx\0" for a token flag */
+const char *token_ckf2str(CK_ULONG flag);
+
+/* mecha_ckf2str - Return string buffer "CKF_xxx\0" for a mechanism flags */
+const char *mecha_ckf2str(CK_ULONG flag);
+
 #endif /*LIBCKTEEC_CK_DEBUG_H*/
