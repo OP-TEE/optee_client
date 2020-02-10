4125cc496e833bc0e8e2876db266b1df19953537
Etienne Carriere <etienne.carriere@linaro.org>
2020-02-10 17:57:34 +0100

    libckteec: get cryptoki return value from the TA
    PKCS11 TA outputs a return value together with the GPD TEE return
    code. When GPD TEE return code from the TA is TEEC_SUCCESS then
    param#0 which is always an output memory reference provides a fine
    return value the library can convert into a compliant PKCS#11
    CKR_xxx ID.
    
    This change updates ping command and generic ckteec_invoke_ta()
    accordingly.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

--- libckteec/src/invoke_ta.c
+++ libckteec/src/invoke_ta.c
@@ -115,14 +115,25 @@ CK_RV ckteec_invoke_ta(unsigned long cmd, TEEC_SharedMemory *ctrl,
 {
 	uint32_t command = (uint32_t)cmd;
 	TEEC_Operation op;
-	uint32_t origin;
-	TEEC_Result res;
+	uint32_t origin = 0;
+	TEEC_Result res = TEEC_ERROR_GENERIC;
+	uint32_t ta_rc = PKCS11_CKR_GENERAL_ERROR;
 
 	memset(&op, 0, sizeof(op));
+
+	if (ctrl && !(ctrl->flags & TEEC_MEM_INPUT &&
+		      ctrl->flags & TEEC_MEM_OUTPUT))
+		return CKR_ARGUMENTS_BAD;
 
 	if (ctrl) {
 		op.paramTypes |= TEEC_PARAM_TYPES(TEEC_MEMREF_WHOLE, 0, 0, 0);
 		op.params[0].memref.parent = ctrl;
+	} else {
+		/* TA mandates param#0 as in/out memref for output status */
+		op.paramTypes |= TEEC_PARAM_TYPES(TEEC_MEMREF_TEMP_INOUT,
+						  0, 0, 0);
+		op.params[0].tmpref.buffer = &ta_rc;
+		op.params[0].tmpref.size = sizeof(ta_rc);
 	}
 
 	if (io1) {
@@ -141,8 +152,19 @@ CK_RV ckteec_invoke_ta(unsigned long cmd, TEEC_SharedMemory *ctrl,
 	}
 
 	res = TEEC_InvokeCommand(&ta_ctx.session, command, &op, &origin);
+	if (res)
+		return teec2ck_rv(res);
 
-	return teec2ck_rv(res);
+	/* Get PKCS11 TA return value from ctrl buffer */
+	if (ctrl) {
+		if (op.params[0].memref.size == sizeof(ta_rc))
+			memcpy(&ta_rc, ctrl->buffer, sizeof(ta_rc));
+	} else {
+		if (op.params[0].tmpref.size != sizeof(ta_rc))
+			ta_rc = PKCS11_CKR_GENERAL_ERROR;
+	}
+
+	return ta_rc;
 }
 
 static CK_RV ping_ta(void)
@@ -151,16 +173,23 @@ static CK_RV ping_ta(void)
 	uint32_t origin;
 	TEEC_Result res;
 	uint32_t ta_version[3];
+	uint32_t status;
 
 	memset(&op, 0, sizeof(op));
+	op.params[0].tmpref.buffer = &status;
+	op.params[0].tmpref.size = sizeof(status);
 	op.params[2].tmpref.buffer = ta_version;
 	op.params[2].tmpref.size = sizeof(ta_version);
-	op.paramTypes = TEEC_PARAM_TYPES(TEEC_NONE, TEEC_NONE,
+	op.paramTypes = TEEC_PARAM_TYPES(TEEC_MEMREF_TEMP_INOUT, TEEC_NONE,
 					 TEEC_MEMREF_TEMP_OUTPUT, TEEC_NONE);
 
 	res = TEEC_InvokeCommand(&ta_ctx.session, PKCS11_CMD_PING, &op,
 				 &origin);
-	if (res != TEEC_SUCCESS)
+
+	if (res != TEEC_SUCCESS ||
+	    origin != TEEC_ORIGIN_TRUSTED_APP ||
+	    op.params[0].tmpref.size != sizeof(status) ||
+	    status != PKCS11_CKR_OK)
 		return CKR_DEVICE_ERROR;
 
 	if (ta_version[0] != PKCS11_TA_VERSION_MAJOR &&
