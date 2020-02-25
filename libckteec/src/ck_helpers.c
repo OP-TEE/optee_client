OP-TEE/optee_client

commit 1e04717835ba580208d5db8514eb09e3f059da8a

libckteec: helpers for Cryptoki/PKCS11 TA IDs conversion
The PKCS11 TA uses IDs that mostly relate to defined PKCS#11 IDs
but with different numerical value. These helpers convert PKCS#11
IDs into/from PKCS11 TA IDs.

This change introduces conversion from GPD TEE Client error codes
into CryptoKi return values.

Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

libckteec/src/ck_helpers.c
@@ -1,0 +1,22 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (c) 2020, Linaro Limited
+ */
+
+#include "ck_helpers.h"
+
+CK_RV teec2ck_rv(TEEC_Result res)
+{
+	switch (res) {
+	case TEEC_SUCCESS:
+		return CKR_OK;
+	case TEEC_ERROR_OUT_OF_MEMORY:
+		return CKR_DEVICE_MEMORY;
+	case TEEC_ERROR_BAD_PARAMETERS:
+		return CKR_ARGUMENTS_BAD;
+	case TEEC_ERROR_SHORT_BUFFER:
+		return CKR_BUFFER_TOO_SMALL;
+	default:
+		return CKR_FUNCTION_FAILED;
+	}
+}

libckteec/CMakeLists.txt
@@ -17,6 +17,7 @@ include(GNUInstallDirs)
 ################################################################################
 set (SRC
 	src/pkcs11_api.c
+	src/ck_helpers.c
 )
 
 ################################################################################

libckteec/Makefile
@@ -22,6 +22,7 @@ LIBCKTEEC_AR_LIBRARY	:= $(LIB_NAME).a
 LIBCKTEEC_SRC_DIR	:= src
 
 LIBCKTEEC_SRCS		= pkcs11_api.c
+LIBCKTEEC_SRCS		+= ck_helpers.c
 
 LIBCKTEEC_INCLUDES	= ${CURDIR}/include
 LIBCKTEEC_INCLUDES 	+= ${CURDIR}/../public

libckteec/src/ck_helpers.h
@@ -1,0 +1,17 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (c) 2020, Linaro Limited
+ */
+
+#ifndef LIBCKTEEC_CK_HELPERS_H
+#define LIBCKTEEC_CK_HELPERS_H
+
+#include <pkcs11.h>
+#include <tee_client_api.h>
+
+/*
+ * Convert IDs between PKCS11 TA and Cryptoki.
+ */
+CK_RV teec2ck_rv(TEEC_Result res);
+
+#endif /*LIBCKTEEC_CK_HELPERS_H*/

commit ff39ddd645e6d37952379dbfb4552d7e3cd7c65f

libckteec: fix CK return value on unexpected TEE error
When TA returns with an unexpected status, CK library shall report
error CKR_DEVICE_ERROR rather than CKR_FUNCTION_FAILED.

As teec2ck_rv() is used only from invoke_ta(), remove it from
ck_helpers.c.

Fixes: 1e04717835ba ("libckteec: helpers for Cryptoki/PKCS11 TA IDs conversion")
Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

libckteec/src/ck_helpers.c
@@ -10,22 +10,6 @@
 
 #include "ck_helpers.h"
 
-CK_RV teec2ck_rv(TEEC_Result res)
-{
-	switch (res) {
-	case TEEC_SUCCESS:
-		return CKR_OK;
-	case TEEC_ERROR_OUT_OF_MEMORY:
-		return CKR_DEVICE_MEMORY;
-	case TEEC_ERROR_BAD_PARAMETERS:
-		return CKR_ARGUMENTS_BAD;
-	case TEEC_ERROR_SHORT_BUFFER:
-		return CKR_BUFFER_TOO_SMALL;
-	default:
-		return CKR_FUNCTION_FAILED;
-	}
-}
-
 #ifdef DEBUG
 void ckteec_assert_expected_rv(const char *function, CK_RV rv,
 			       const CK_RV *expected_rv, size_t expected_count)

libckteec/src/invoke_ta.c
@@ -152,8 +152,16 @@ CK_RV ckteec_invoke_ta(unsigned long cmd, TEEC_SharedMemory *ctrl,
 	}
 
 	res = TEEC_InvokeCommand(&ta_ctx.session, command, &op, &origin);
-	if (res)
-		return teec2ck_rv(res);
+	switch (res) {
+	case TEEC_SUCCESS:
+		break;
+	case TEEC_ERROR_SHORT_BUFFER:
+		return CKR_BUFFER_TOO_SMALL;
+	case TEEC_ERROR_OUT_OF_MEMORY:
+		return CKR_DEVICE_MEMORY;
+	default:
+		return CKR_GENERAL_ERROR;
+	}
 
 	/* Get PKCS11 TA return value from ctrl buffer */
 	if (ctrl) {