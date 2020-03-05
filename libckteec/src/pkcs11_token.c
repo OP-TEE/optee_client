OP-TEE/optee_client

commit 4dc4081da3e3891db3b2ba7a66b4423f15df7cfb

libckteec: implement C_GetSlotList()
Implement C_GetSlotList() API function for client to retrieve the IDs
if the slots implemented by the PKCS11 TA.

Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

libckteec/src/pkcs11_token.c
@@ -55,3 +55,46 @@ CK_RV ck_get_info(CK_INFO_PTR info)
 
 	return CKR_OK;
 }
+
+/**
+ * ck_slot_get_list - Wrap C_GetSlotList into PKCS11_CMD_SLOT_LIST
+ */
+CK_RV ck_slot_get_list(CK_BBOOL present,
+		       CK_SLOT_ID_PTR slots, CK_ULONG_PTR count)
+{
+	CK_RV rv = CKR_GENERAL_ERROR;
+	TEEC_SharedMemory *shm = NULL;
+	uint32_t *slot_ids = NULL;
+	size_t size = 0;
+
+	/* Discard @present: all slots reported by TA are present */
+	(void)present;
+
+	if (!count || (*count && !slots))
+		return CKR_ARGUMENTS_BAD;
+
+	size = *count * sizeof(*slot_ids);
+
+	shm = ckteec_alloc_shm(size, CKTEEC_SHM_OUT);
+	if (!shm)
+		return CKR_HOST_MEMORY;
+
+	rv = ckteec_invoke_ta(PKCS11_CMD_SLOT_LIST, NULL,
+			      NULL, shm, &size, NULL, NULL);
+
+	if (rv == CKR_OK || rv == CKR_BUFFER_TOO_SMALL) {
+		*count = size / sizeof(*slot_ids);
+
+		if (rv == CKR_OK && slots) {
+			size_t n = 0;
+
+			slot_ids = shm->buffer;
+			for (n = 0; n < *count; n++)
+				slots[n] = slot_ids[n];
+		}
+	}
+
+	ckteec_free_shm(shm);
+
+	return rv;
+}

libckteec/src/pkcs11_api.c
@@ -20,6 +20,7 @@ static const CK_FUNCTION_LIST libckteec_function_list = {
 	.C_Finalize = C_Finalize,
 	.C_GetInfo = C_GetInfo,
 	.C_GetFunctionList = C_GetFunctionList,
+	.C_GetSlotList = C_GetSlotList,
 };
 
 static bool lib_initiated(void)
@@ -90,14 +91,16 @@ CK_RV C_GetSlotList(CK_BBOOL tokenPresent,
 		    CK_SLOT_ID_PTR pSlotList,
 		    CK_ULONG_PTR pulCount)
 {
-	(void)tokenPresent;
-	(void)pSlotList;
-	(void)pulCount;
+	CK_RV rv = CKR_CRYPTOKI_NOT_INITIALIZED;
 
-	if (!lib_initiated())
-		return CKR_CRYPTOKI_NOT_INITIALIZED;
+	if (lib_initiated())
+		rv = ck_slot_get_list(tokenPresent, pSlotList, pulCount);
 
-	return CKR_FUNCTION_NOT_SUPPORTED;
+	ASSERT_CK_RV(rv, CKR_ARGUMENTS_BAD, CKR_BUFFER_TOO_SMALL,
+		     CKR_CRYPTOKI_NOT_INITIALIZED, CKR_FUNCTION_FAILED,
+		     CKR_GENERAL_ERROR, CKR_HOST_MEMORY, CKR_OK);
+
+	return rv;
 }
 
 CK_RV C_GetSlotInfo(CK_SLOT_ID slotID,

libckteec/src/pkcs11_token.h
@@ -12,4 +12,7 @@
 
 CK_RV ck_get_info(CK_INFO_PTR info);
 
+CK_RV ck_slot_get_list(CK_BBOOL present,
+		       CK_SLOT_ID_PTR slots, CK_ULONG_PTR count);
+
 #endif /*LIBCKTEEC_PKCS11_TOKEN_H*/

commit 759e584e86824bdbf0e811e0cce04851deeaa196

libckteec: better handle null size buffer in C_GetSlotList()
When caller of C_GetSlotList() provides a non-NULL buffer reference
with a count value of zero, the TA gets invoked with a null buffer
reference in the OP-TEE shared memory, hence it will return CKR_OK
status since it legitimately only returns the count information. This
change makes ckteec library to return CKR_BUFFER_TOO_SMALL as
per PKCS#11 specification.

This change also fixes C_GetSlotList() that, prior to this change,
would have returned CKR_ARGUMENTS_BAD upon NULL buffer and non-zero
count value whereas specification says that "The contents of *pulCount
on entry to C_GetSlotList has no meaning in this case (...)".

Fixes: 4dc4081da3e3 ("libckteec: implement C_GetSlotList()")
Reported-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>
Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

libckteec/src/pkcs11_token.c
@@ -65,15 +65,23 @@ CK_RV ck_slot_get_list(CK_BBOOL present,
 	CK_RV rv = CKR_GENERAL_ERROR;
 	TEEC_SharedMemory *shm = NULL;
 	uint32_t *slot_ids = NULL;
+	size_t client_count = 0;
 	size_t size = 0;
 
 	/* Discard @present: all slots reported by TA are present */
 	(void)present;
 
-	if (!count || (*count && !slots))
+	if (!count)
 		return CKR_ARGUMENTS_BAD;
 
-	size = *count * sizeof(*slot_ids);
+	/*
+	 * As per spec, if @slots is NULL, "The contents of *pulCount on
+	 * entry to C_GetSlotList has no meaning in this case (...)"
+	 */
+	if (slots)
+		client_count = *count;
+
+	size = client_count * sizeof(*slot_ids);
 
 	shm = ckteec_alloc_shm(size, CKTEEC_SHM_OUT);
 	if (!shm)
@@ -83,6 +91,16 @@ CK_RV ck_slot_get_list(CK_BBOOL present,
 			      NULL, shm, &size, NULL, NULL);
 
 	if (rv == CKR_OK || rv == CKR_BUFFER_TOO_SMALL) {
+		/*
+		 * When @slot != NULL and @count[0] == 0, library shall
+		 * return CKR_BUFFER_TOO_SMALL if provided buffer is too
+		 * small whereas TA would have returned CKR_OK since
+		 * allocating a zero sized shm instance results in a NULL
+		 * shm buffer reference.
+		 */
+		if (size && slots && !client_count)
+			rv = CKR_BUFFER_TOO_SMALL;
+
 		*count = size / sizeof(*slot_ids);
 
 		if (rv == CKR_OK && slots) {