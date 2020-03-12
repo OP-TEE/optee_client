OP-TEE/optee_client

commit 682bde7f5976aa3aa1db5b85fdd9f0350351c1c1

libckteec: implement C_GetSlotInfo()
Implement C_GetSlotInfo() API function to retrieve information about
a slot implemented by the PKCS11 TA.

Helper function ta2ck_slot_info() converts slot information data from
TA API into compliant Cryptoki API.

Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

libckteec/src/pkcs11_token.c
@@ -98,3 +98,69 @@ CK_RV ck_slot_get_list(CK_BBOOL present,
 
 	return rv;
 }
+
+/**
+ * ck_slot_get_info - Wrap C_GetSlotInfo into PKCS11_CMD_SLOT_INFO
+ */
+CK_RV ck_slot_get_info(CK_SLOT_ID slot, CK_SLOT_INFO_PTR info)
+{
+	CK_RV rv = CKR_GENERAL_ERROR;
+	TEEC_SharedMemory *ctrl = NULL;
+	TEEC_SharedMemory *out = NULL;
+	uint32_t slot_id = slot;
+	struct pkcs11_slot_info *ta_info = NULL;
+	size_t out_size = 0;
+
+	if (!info)
+		return CKR_ARGUMENTS_BAD;
+
+	ctrl = ckteec_alloc_shm(sizeof(slot_id), CKTEEC_SHM_INOUT);
+	if (!ctrl) {
+		rv = CKR_HOST_MEMORY;
+		goto bail;
+	}
+	memcpy(ctrl->buffer, &slot_id, sizeof(slot_id));
+
+	out = ckteec_alloc_shm(sizeof(*ta_info), CKTEEC_SHM_OUT);
+	if (!out) {
+		rv = CKR_HOST_MEMORY;
+		goto bail;
+	}
+
+	rv = ckteec_invoke_ctrl_out(PKCS11_CMD_SLOT_INFO, ctrl, out, &out_size);
+	if (rv != CKR_OK || out_size != out->size) {
+		if (rv == CKR_OK)
+			rv = CKR_DEVICE_ERROR;
+		goto bail;
+	}
+
+	ta_info = out->buffer;
+
+	COMPILE_TIME_ASSERT(sizeof(info->slotDescription) ==
+			    sizeof(ta_info->slot_description));
+	memcpy(info->slotDescription, ta_info->slot_description,
+	       sizeof(info->slotDescription));
+
+	COMPILE_TIME_ASSERT(sizeof(info->manufacturerID) ==
+			    sizeof(ta_info->manufacturer_id));
+	memcpy(info->manufacturerID, ta_info->manufacturer_id,
+	       sizeof(info->manufacturerID));
+
+	info->flags = ta_info->flags;
+
+	COMPILE_TIME_ASSERT(sizeof(info->hardwareVersion) ==
+			    sizeof(ta_info->hardware_version));
+	memcpy(&info->hardwareVersion, ta_info->hardware_version,
+	       sizeof(info->hardwareVersion));
+
+	COMPILE_TIME_ASSERT(sizeof(info->firmwareVersion) ==
+			    sizeof(ta_info->firmware_version));
+	memcpy(&info->firmwareVersion, ta_info->firmware_version,
+	       sizeof(info->firmwareVersion));
+
+bail:
+	ckteec_free_shm(ctrl);
+	ckteec_free_shm(out);
+
+	return rv;
+}

libckteec/src/local_utils.h
@@ -8,4 +8,9 @@
 
 #define ARRAY_SIZE(array)	(sizeof(array) / sizeof(array[0]))
 
+#define COMPILE_TIME_ASSERT(x) \
+	do { \
+		switch (0) { case 0: case ((x) ? 1: 0) : default : break; } \
+	} while (0)
+
 #endif /*LIBCKTEEC_LOCAL_UTILS_H*/

libckteec/src/pkcs11_api.c
@@ -21,6 +21,7 @@ static const CK_FUNCTION_LIST libckteec_function_list = {
 	.C_GetInfo = C_GetInfo,
 	.C_GetFunctionList = C_GetFunctionList,
 	.C_GetSlotList = C_GetSlotList,
+	.C_GetSlotInfo = C_GetSlotInfo,
 };
 
 static bool lib_initiated(void)
@@ -106,13 +107,16 @@ CK_RV C_GetSlotList(CK_BBOOL tokenPresent,
 CK_RV C_GetSlotInfo(CK_SLOT_ID slotID,
 		    CK_SLOT_INFO_PTR pInfo)
 {
-	(void)slotID;
-	(void)pInfo;
+	CK_RV rv = CKR_CRYPTOKI_NOT_INITIALIZED;
 
-	if (!lib_initiated())
-		return CKR_CRYPTOKI_NOT_INITIALIZED;
+	if (lib_initiated())
+		rv = ck_slot_get_info(slotID, pInfo);
 
-	return CKR_FUNCTION_NOT_SUPPORTED;
+	ASSERT_CK_RV(rv, CKR_ARGUMENTS_BAD, CKR_CRYPTOKI_NOT_INITIALIZED,
+		     CKR_DEVICE_ERROR, CKR_FUNCTION_FAILED, CKR_GENERAL_ERROR,
+		     CKR_HOST_MEMORY, CKR_OK, CKR_SLOT_ID_INVALID);
+
+	return rv;
 }
 
 CK_RV C_InitToken(CK_SLOT_ID slotID,

libckteec/src/pkcs11_token.h
@@ -15,4 +15,6 @@ CK_RV ck_get_info(CK_INFO_PTR info);
 CK_RV ck_slot_get_list(CK_BBOOL present,
 		       CK_SLOT_ID_PTR slots, CK_ULONG_PTR count);
 
+CK_RV ck_slot_get_info(CK_SLOT_ID slot, CK_SLOT_INFO_PTR info);
+
 #endif /*LIBCKTEEC_PKCS11_TOKEN_H*/

commit b2e3e016020ee574a122625d346f499ff4ad6131

libckteec: rename label bail into out in command handlers
Prefer label out over bail for fast exit path in TA command handlers.

Fixes: commit 682bde7f5976 ("libckteec: implement C_GetSlotInfo()")
Fixes: commit c8453190a8cd ("libckteec: implement C_GetTokenInfo()")
Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

libckteec/src/pkcs11_token.c
@@ -135,21 +135,21 @@ CK_RV ck_slot_get_info(CK_SLOT_ID slot, CK_SLOT_INFO_PTR info)
 	ctrl = ckteec_alloc_shm(sizeof(slot_id), CKTEEC_SHM_INOUT);
 	if (!ctrl) {
 		rv = CKR_HOST_MEMORY;
-		goto bail;
+		goto out;
 	}
 	memcpy(ctrl->buffer, &slot_id, sizeof(slot_id));
 
 	out = ckteec_alloc_shm(sizeof(*ta_info), CKTEEC_SHM_OUT);
 	if (!out) {
 		rv = CKR_HOST_MEMORY;
-		goto bail;
+		goto out;
 	}
 
 	rv = ckteec_invoke_ctrl_out(PKCS11_CMD_SLOT_INFO, ctrl, out, &out_size);
 	if (rv != CKR_OK || out_size != out->size) {
 		if (rv == CKR_OK)
 			rv = CKR_DEVICE_ERROR;
-		goto bail;
+		goto out;
 	}
 
 	ta_info = out->buffer;
@@ -176,7 +176,7 @@ CK_RV ck_slot_get_info(CK_SLOT_ID slot, CK_SLOT_INFO_PTR info)
 	memcpy(&info->firmwareVersion, ta_info->firmware_version,
 	       sizeof(info->firmwareVersion));
 
-bail:
+out:
 	ckteec_free_shm(ctrl);
 	ckteec_free_shm(out);
 
@@ -201,24 +201,24 @@ CK_RV ck_token_get_info(CK_SLOT_ID slot, CK_TOKEN_INFO_PTR info)
 	ctrl = ckteec_alloc_shm(sizeof(slot_id), CKTEEC_SHM_INOUT);
 	if (!ctrl) {
 		rv = CKR_HOST_MEMORY;
-		goto bail;
+		goto out;
 	}
 	memcpy(ctrl->buffer, &slot_id, sizeof(slot_id));
 
 	out_shm = ckteec_alloc_shm(sizeof(*ta_info), CKTEEC_SHM_OUT);
 	if (!out_shm) {
 		rv = CKR_HOST_MEMORY;
-		goto bail;
+		goto out;
 	}
 
 	rv = ckteec_invoke_ctrl_out(PKCS11_CMD_TOKEN_INFO, ctrl,
 				    out_shm, &out_size);
 	if (rv)
-		goto bail;
+		goto out;
 
 	if (out_size != out_shm->size) {
 		rv = CKR_DEVICE_ERROR;
-		goto bail;
+		goto out;
 	}
 
 	ta_info = out_shm->buffer;
@@ -264,7 +264,7 @@ CK_RV ck_token_get_info(CK_SLOT_ID slot, CK_TOKEN_INFO_PTR info)
 	COMPILE_TIME_ASSERT(sizeof(info->utcTime) == sizeof(ta_info->utc_time));
 	memcpy(&info->utcTime, ta_info->utc_time, sizeof(info->utcTime));
 
-bail:
+out:
 	ckteec_free_shm(ctrl);
 	ckteec_free_shm(out_shm);
 