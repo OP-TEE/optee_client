c3491290280f7f895191793a5ec27854c93f422e
Etienne Carriere <etienne.carriere@linaro.org>
2020-04-21 12:09:29 +0200

    libckteec: introduce helpers for serializing data
    New helper functions to serialize data provided by client into
    attribute list structure with a header and followed by the
    serialized item. Each serialized item is made of a item ID,
    an item value size followed by the item binary value as a byte
    array.
    
    init_serial_object() initializes a serial object.
    
    finalize_serial_object() sets attributes meta data (byte size,
    item count) in attribute list header.
    
    release_serial_object() frees resources allocated by serialization
    process.
    
    serialize_buffer() appends an item of variable size value.
    serialize_32b() appends a item of 32bit size value.
    
    Acked-by: Jerome Forissier <jerome@forissier.org>
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

---
+++ libckteec/src/serializer.h
@@ -0,0 +1,31 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (c) 2017-2020, Linaro Limited
+ */
+#ifndef LIBCKTEEC_SERIALIZER_H
+#define LIBCKTEEC_SERIALIZER_H
+
+#include <pkcs11.h>
+#include <pkcs11_ta.h>
+#include <stddef.h>
+#include <stdint.h>
+
+/*
+ * Struct used to create the buffer storing the serialized data.
+ * Contains some fields to help parsing content (type/boolprops).
+ */
+struct serializer {
+	char *buffer;		/* serial buffer base address */
+	size_t size;		/* serial buffer current byte size */
+	size_t item_count;	/* number of items in entry table */
+};
+
+/* Init/finalize/release a serializer object */
+CK_RV init_serial_object(struct serializer *obj);
+void finalize_serial_object(struct serializer *obj);
+void release_serial_object(struct serializer *obj);
+
+CK_RV serialize_buffer(struct serializer *obj, void *data, size_t size);
+CK_RV serialize_32b(struct serializer *obj, uint32_t data);
+
+#endif /*LIBCKTEEC_SERIALIZER_H*/
