85a7ea7861339356b39c73926f2b5cb9added45b
Etienne Carriere <etienne.carriere@linaro.org>
2020-04-21 12:09:29 +0200

    libckteec: introduce helpers for serializing data
    New helper functions to serialize data provided by client into
    attribute list structure with a header and followed by the
    serialized item. Each serialized item is made of a item ID,
    an item value size followed by the item binary value as a byte
    array.
    
    init_serial_object() initializes a serial object.
    
    finalize_serial_object() sets attributes meta data (byte size,
    item count) in attribute list header.
    
    release_serial_object() frees resources allocated by serialization
    process.
    
    serialize_buffer() appends an item of variable size value.
    serialize_32b() appends a item of 32bit size value.
    
    Acked-by: Jerome Forissier <jerome@forissier.org>
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

---
+++ libckteec/src/serializer.c
@@ -0,0 +1,69 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (c) 2017-2020, Linaro Limited
+ */
+
+#include <pkcs11_ta.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "ck_helpers.h"
+#include "local_utils.h"
+#include "serializer.h"
+
+CK_RV init_serial_object(struct serializer *obj)
+{
+	struct pkcs11_object_head head = { };
+
+	memset(obj, 0, sizeof(*obj));
+
+	return serialize_buffer(obj, &head, sizeof(head));
+}
+
+void finalize_serial_object(struct serializer *obj)
+{
+	struct pkcs11_object_head head = { };
+
+	head.attrs_size = obj->size - sizeof(head);
+	head.attrs_count = obj->item_count;
+	memcpy(obj->buffer, &head, sizeof(head));
+}
+
+void release_serial_object(struct serializer *obj)
+{
+	free(obj->buffer);
+	obj->buffer = NULL;
+}
+
+/**
+ * serialize - append data in a serialized buffer
+ *
+ * Serialize data in provided buffer.
+ * Ensure 64byte alignment of appended data in the buffer.
+ */
+static CK_RV serialize(char **bstart, size_t *blen, void *data, size_t len)
+{
+	size_t nlen = *blen + len;
+	char *buf = realloc(*bstart, nlen);
+
+	if (!buf)
+		return CKR_HOST_MEMORY;
+
+	memcpy(buf + *blen, data, len);
+
+	*blen = nlen;
+	*bstart = buf;
+
+	return CKR_OK;
+}
+
+CK_RV serialize_buffer(struct serializer *obj, void *data, size_t size)
+{
+	return serialize(&obj->buffer, &obj->size, data, size);
+}
+
+CK_RV serialize_32b(struct serializer *obj, uint32_t data)
+{
+	return serialize_buffer(obj, &data, sizeof(data));
+}
