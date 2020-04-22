e06e7a46074be9671dfd2158ecfc1ecdc26f05e6
Etienne Carriere <etienne.carriere@linaro.org>
2020-04-22 16:40:39 +0200

    libckteec: fix C_GetMechanismList() when output list buffer is NULL
    Prevent useless allocation as per PKCS11 specification, content pulCount
    has no meaning if pMechanimList is NULL.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

--- libckteec/src/pkcs11_token.c
+++ libckteec/src/pkcs11_token.c
@@ -278,10 +278,15 @@ CK_RV ck_token_mechanism_ids(CK_SLOT_ID slot,
 	size_t out_size = 0;
 	size_t n = 0;
 
-	if (!count || (*count && !mechanisms))
+	if (!count)
 		return CKR_ARGUMENTS_BAD;
 
-	out_size = *count * sizeof(*mecha_ids);
+	/*
+	 * As per spec, if @mechanism is NULL, "The contents of *pulCount on
+	 * entry to C_GetMechanismList has no meaning in this case (...)"
+	 */
+	if (mechanisms)
+		out_size = *count * sizeof(*mecha_ids);
 
 	ctrl = ckteec_alloc_shm(sizeof(slot_id), CKTEEC_SHM_INOUT);
 	if (!ctrl) {
