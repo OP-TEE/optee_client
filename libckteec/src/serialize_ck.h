110d11d7afedef54bc0245889f54dd303a7b5c7a
Ruchika Gupta <ruchika.gupta@linaro.org>
2020-12-21 15:00:54 +0530

    libckteec: helper function to deserialize a attribute template
    Helper function deserialize_ck_attributes() takes a serial list of
    attributes and generates an attribute list.
    
    Co-developed-by: Etienne Carriere <etienne.carriere@linaro.org>
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
    Co-developed-by: Gabor Szekely <szvgabor@gmail.com>
    Signed-off-by: Gabor Szekely <szvgabor@gmail.com>
    Signed-off-by: Ruchika Gupta <ruchika.gupta@linaro.org>
    Reviewed-by: Vesa Jääskeläinen <vesa.jaaskelainen@vaisala.com>

--- libckteec/src/serialize_ck.h
+++ libckteec/src/serialize_ck.h
@@ -13,6 +13,10 @@
 CK_RV serialize_ck_attributes(struct serializer *obj,
 			      CK_ATTRIBUTE_PTR attributes, CK_ULONG count);
 
+/* Convert PKCS11 TA attributes back to CK_ATTRIBUTE array */
+CK_RV deserialize_ck_attributes(uint8_t *in,
+				CK_ATTRIBUTE_PTR attributes, CK_ULONG count);
+
 /* Create (and allocate) a serial object for CK_MECHANISM array */
 CK_RV serialize_ck_mecha_params(struct serializer *obj,
 				CK_MECHANISM_PTR mechanisms);
