OP-TEE/optee_client

commit 3f44b870299514ad8c6b7dee776eb2994d9c1cd4

libckteec: add support for Signing and Verification Functions
Adds support for following API's :
- C_SignInit()
- C_Sign()
- C_SignUpdate()
- C_SignFinal()
- C_VerifyInit()
- C_Verify()
- C_VerifyUpdate()
- C_VerifyFinal()

Co-developed-by: Etienne Carriere <etienne.carriere@linaro.org>
Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
Signed-off-by: Ruchika Gupta <ruchika.gupta@linaro.org>
Reviewed-by: Ricardo Salveti <ricardo@foundries.io>

libckteec/src/pkcs11_api.c
@@ -859,14 +859,23 @@ CK_RV C_SignInit(CK_SESSION_HANDLE hSession,
 		 CK_MECHANISM_PTR pMechanism,
 		 CK_OBJECT_HANDLE hKey)
 {
-	(void)hSession;
-	(void)pMechanism;
-	(void)hKey;
+	CK_RV rv = CKR_CRYPTOKI_NOT_INITIALIZED;
 
-	if (!lib_initiated())
-		return CKR_CRYPTOKI_NOT_INITIALIZED;
+	if (lib_initiated())
+		rv = ck_signverify_init(hSession, pMechanism, hKey, CK_TRUE);
 
-	return CKR_FUNCTION_NOT_SUPPORTED;
+	ASSERT_CK_RV(rv, CKR_ARGUMENTS_BAD, CKR_CRYPTOKI_NOT_INITIALIZED,
+		     CKR_DEVICE_ERROR, CKR_DEVICE_MEMORY, CKR_DEVICE_REMOVED,
+		     CKR_FUNCTION_CANCELED, CKR_FUNCTION_FAILED,
+		     CKR_GENERAL_ERROR, CKR_HOST_MEMORY,
+		     CKR_KEY_FUNCTION_NOT_PERMITTED, CKR_KEY_HANDLE_INVALID,
+		     CKR_KEY_SIZE_RANGE, CKR_KEY_TYPE_INCONSISTENT,
+		     CKR_MECHANISM_INVALID, CKR_MECHANISM_PARAM_INVALID,
+		     CKR_OK, CKR_OPERATION_ACTIVE, CKR_PIN_EXPIRED,
+		     CKR_SESSION_CLOSED, CKR_SESSION_HANDLE_INVALID,
+		     CKR_USER_NOT_LOGGED_IN);
+
+	return rv;
 }
 
 CK_RV C_Sign(CK_SESSION_HANDLE hSession,
@@ -875,44 +884,64 @@ CK_RV C_Sign(CK_SESSION_HANDLE hSession,
 	     CK_BYTE_PTR pSignature,
 	     CK_ULONG_PTR pulSignatureLen)
 {
-	(void)hSession;
-	(void)pData;
-	(void)ulDataLen;
-	(void)pSignature;
-	(void)pulSignatureLen;
+	CK_RV rv = CKR_CRYPTOKI_NOT_INITIALIZED;
 
-	if (!lib_initiated())
-		return CKR_CRYPTOKI_NOT_INITIALIZED;
+	if (lib_initiated())
+		rv = ck_signverify_oneshot(hSession, pData, ulDataLen,
+					   pSignature, pulSignatureLen,
+					   CK_TRUE);
 
-	return CKR_FUNCTION_NOT_SUPPORTED;
+	ASSERT_CK_RV(rv, CKR_ARGUMENTS_BAD, CKR_BUFFER_TOO_SMALL,
+		     CKR_CRYPTOKI_NOT_INITIALIZED, CKR_DATA_INVALID,
+		     CKR_DATA_LEN_RANGE, CKR_DEVICE_ERROR, CKR_DEVICE_MEMORY,
+		     CKR_DEVICE_REMOVED, CKR_FUNCTION_CANCELED,
+		     CKR_FUNCTION_FAILED, CKR_GENERAL_ERROR, CKR_HOST_MEMORY,
+		     CKR_OK, CKR_OPERATION_NOT_INITIALIZED, CKR_SESSION_CLOSED,
+		     CKR_SESSION_HANDLE_INVALID, CKR_USER_NOT_LOGG_IN,
+		     CKR_FUNCTION_REJECTED);
+
+	return rv;
 }
 
 CK_RV C_SignUpdate(CK_SESSION_HANDLE hSession,
 		   CK_BYTE_PTR pPart,
 		   CK_ULONG ulPartLen)
 {
-	(void)hSession;
-	(void)pPart;
-	(void)ulPartLen;
+	CK_RV rv = CKR_CRYPTOKI_NOT_INITIALIZED;
 
-	if (!lib_initiated())
-		return CKR_CRYPTOKI_NOT_INITIALIZED;
+	if (lib_initiated())
+		rv = ck_signverify_update(hSession, pPart, ulPartLen, CK_TRUE);
 
-	return CKR_FUNCTION_NOT_SUPPORTED;
+	ASSERT_CK_RV(rv, CKR_ARGUMENTS_BAD, CKR_CRYPTOKI_NOT_INITIALIZED,
+		     CKR_DATA_LEN_RANGE, CKR_DEVICE_ERROR, CKR_DEVICE_MEMORY,
+		     CKR_DEVICE_REMOVED, CKR_FUNCTION_CANCELED,
+		     CKR_FUNCTION_FAILED, CKR_GENERAL_ERROR, CKR_HOST_MEMORY,
+		     CKR_OK, CKR_OPERATION_NOT_INITIALIZED, CKR_SESSION_CLOSED,
+		     CKR_SESSION_HANDLE_INVALID, CKR_USER_NOT_LOGGED_IN);
+
+	return rv;
 }
 
 CK_RV C_SignFinal(CK_SESSION_HANDLE hSession,
 		  CK_BYTE_PTR pSignature,
 		  CK_ULONG_PTR pulSignatureLen)
 {
-	(void)hSession;
-	(void)pSignature;
-	(void)pulSignatureLen;
+	CK_RV rv = CKR_CRYPTOKI_NOT_INITIALIZED;
 
-	if (!lib_initiated())
-		return CKR_CRYPTOKI_NOT_INITIALIZED;
+	if (lib_initiated())
+		rv = ck_signverify_final(hSession, pSignature, pulSignatureLen,
+					 CK_TRUE);
 
-	return CKR_FUNCTION_NOT_SUPPORTED;
+	ASSERT_CK_RV(rv, CKR_ARGUMENTS_BAD, CKR_BUFFER_TOO_SMALL,
+		     CKR_CRYPTOKI_NOT_INITIALIZED, CKR_DATA_LEN_RANGE,
+		     CKR_DEVICE_ERROR, CKR_DEVICE_MEMORY, CKR_DEVICE_REMOVED,
+		     CKR_FUNCTION_CANCELED, CKR_FUNCTION_FAILED,
+		     CKR_GENERAL_ERROR, CKR_HOST_MEMORY, CKR_OK,
+		     CKR_OPERATION_NOT_INITIALIZED, CKR_SESSION_CLOSED,
+		     CKR_SESSION_HANDLE_INVALID, CKR_USER_NOT_LOGGED_IN,
+		     CKR_FUNCTION_REJECTED);
+
+	return rv;
 }
 
 CK_RV C_SignRecoverInit(CK_SESSION_HANDLE hSession,
@@ -951,14 +980,23 @@ CK_RV C_VerifyInit(CK_SESSION_HANDLE hSession,
 		   CK_MECHANISM_PTR pMechanism,
 		   CK_OBJECT_HANDLE hKey)
 {
-	(void)hSession;
-	(void)pMechanism;
-	(void)hKey;
+	CK_RV rv = CKR_CRYPTOKI_NOT_INITIALIZED;
 
-	if (!lib_initiated())
-		return CKR_CRYPTOKI_NOT_INITIALIZED;
+	if (lib_initiated())
+		rv = ck_signverify_init(hSession, pMechanism, hKey, CK_FALSE);
 
-	return CKR_FUNCTION_NOT_SUPPORTED;
+	ASSERT_CK_RV(rv, CKR_ARGUMENTS_BAD, CKR_CRYPTOKI_NOT_INITIALIZED,
+		     CKR_DEVICE_ERROR, CKR_DEVICE_MEMORY, CKR_DEVICE_REMOVED,
+		     CKR_FUNCTION_CANCELED, CKR_FUNCTION_FAILED,
+		     CKR_GENERAL_ERROR, CKR_HOST_MEMORY,
+		     CKR_KEY_FUNCTION_NOT_PERMITTED, CKR_KEY_HANDLE_INVALID,
+		     CKR_KEY_SIZE_RANGE, CKR_KEY_TYPE_INCONSISTENT,
+		     CKR_MECHANISM_INVALID, CKR_MECHANISM_PARAM_INVALID,
+		     CKR_OK, CKR_OPERATION_ACTIVE, CKR_PIN_EXPIRED,
+		     CKR_SESSION_CLOSED, CKR_SESSION_HANDLE_INVALID,
+		     CKR_USER_NOT_LOGGED_IN);
+
+	return rv;
 }
 
 CK_RV C_Verify(CK_SESSION_HANDLE hSession,
@@ -967,44 +1005,64 @@ CK_RV C_Verify(CK_SESSION_HANDLE hSession,
 	       CK_BYTE_PTR pSignature,
 	       CK_ULONG ulSignatureLen)
 {
-	(void)hSession;
-	(void)pData;
-	(void)ulDataLen;
-	(void)pSignature;
-	(void)ulSignatureLen;
+	CK_RV rv = CKR_CRYPTOKI_NOT_INITIALIZED;
+	CK_ULONG out_size = ulSignatureLen;
 
-	if (!lib_initiated())
-		return CKR_CRYPTOKI_NOT_INITIALIZED;
+	if (lib_initiated())
+		rv = ck_signverify_oneshot(hSession, pData, ulDataLen,
+					   pSignature, &out_size,
+					   CK_FALSE);
 
-	return CKR_FUNCTION_NOT_SUPPORTED;
+	ASSERT_CK_RV(rv, CKR_ARGUMENTS_BAD, CKR_CRYPTOKI_NOT_INITIALIZED,
+		     CKR_DATA_INVALID, CKR_DATA_LEN_RANGE, CKR_DEVICE_ERROR,
+		     CKR_DEVICE_MEMORY, CKR_DEVICE_REMOVED,
+		     CKR_FUNCTION_CANCELED, CKR_FUNCTION_FAILED,
+		     CKR_GENERAL_ERROR, CKR_HOST_MEMORY, CKR_OK,
+		     CKR_OPERATION_NOT_INITIALIZED, CKR_SESSION_CLOSED,
+		     CKR_SESSION_HANDLE_INVALID, CKR_SIGNATURE_INVALID,
+		     CKR_SIGNATURE_LEN_RANGE);
+
+	return rv;
 }
 
 CK_RV C_VerifyUpdate(CK_SESSION_HANDLE hSession,
 		     CK_BYTE_PTR pPart,
 		     CK_ULONG ulPartLen)
 {
-	(void)hSession;
-	(void)pPart;
-	(void)ulPartLen;
+	CK_RV rv = CKR_CRYPTOKI_NOT_INITIALIZED;
 
-	if (!lib_initiated())
-		return CKR_CRYPTOKI_NOT_INITIALIZED;
+	if (lib_initiated())
+		rv = ck_signverify_update(hSession, pPart, ulPartLen, CK_FALSE);
 
-	return CKR_FUNCTION_NOT_SUPPORTED;
+	ASSERT_CK_RV(rv, CKR_ARGUMENTS_BAD, CKR_CRYPTOKI_NOT_INITIALIZED,
+		     CKR_DATA_LEN_RANGE, CKR_DEVICE_ERROR, CKR_DEVICE_MEMORY,
+		     CKR_DEVICE_REMOVED, CKR_FUNCTION_CANCELED,
+		     CKR_FUNCTION_FAILED, CKR_GENERAL_ERROR, CKR_HOST_MEMORY,
+		     CKR_OK, CKR_OPERATION_NOT_INITIALIZED, CKR_SESSION_CLOSED,
+		     CKR_SESSION_HANDLE_INVALID);
+
+	return rv;
 }
 
 CK_RV C_VerifyFinal(CK_SESSION_HANDLE hSession,
 		    CK_BYTE_PTR pSignature,
 		    CK_ULONG ulSignatureLen)
 {
-	(void)hSession;
-	(void)pSignature;
-	(void)ulSignatureLen;
+	CK_RV rv = CKR_CRYPTOKI_NOT_INITIALIZED;
 
-	if (!lib_initiated())
-		return CKR_CRYPTOKI_NOT_INITIALIZED;
+	if (lib_initiated())
+		rv = ck_signverify_final(hSession, pSignature, &ulSignatureLen,
+					 CK_FALSE);
 
-	return CKR_FUNCTION_NOT_SUPPORTED;
+	ASSERT_CK_RV(rv, CKR_ARGUMENTS_BAD, CKR_CRYPTOKI_NOT_INITIALIZED,
+		     CKR_DATA_LEN_RANGE, CKR_DEVICE_ERROR, CKR_DEVICE_MEMORY,
+		     CKR_DEVICE_REMOVED, CKR_FUNCTION_CANCELED,
+		     CKR_FUNCTION_FAILED, CKR_GENERAL_ERROR, CKR_HOST_MEMORY,
+		     CKR_OK, CKR_OPERATION_NOT_INITIALIZED, CKR_SESSION_CLOSED,
+		     CKR_SESSION_HANDLE_INVALID, CKR_SIGNATURE_INVALID,
+		     CKR_SIGNATURE_LEN_RANGE);
+
+	return rv;
 }
 
 CK_RV C_VerifyRecoverInit(CK_SESSION_HANDLE hSession,

libckteec/src/pkcs11_processing.c
@@ -342,3 +342,221 @@ bail:
 
 	return rv;
 }
+
+CK_RV ck_signverify_init(CK_SESSION_HANDLE session,
+			 CK_MECHANISM_PTR mechanism,
+			 CK_OBJECT_HANDLE key,
+			 int sign)
+{
+	CK_RV rv = CKR_GENERAL_ERROR;
+	TEEC_SharedMemory *ctrl = NULL;
+	struct serializer obj = { 0 };
+	uint32_t session_handle = session;
+	uint32_t key_handle = key;
+	size_t ctrl_size = 0;
+	char *buf = NULL;
+
+	if (!mechanism)
+		return CKR_ARGUMENTS_BAD;
+
+	rv = serialize_ck_mecha_params(&obj, mechanism);
+	if (rv)
+		return rv;
+
+	/*
+	 * Shm io0: (in/out) ctrl
+	 * (in) [session-handle][key-handle][serialized-mechanism-blob]
+	 * (out) [status]
+	 */
+	ctrl_size = sizeof(session_handle) + sizeof(key_handle) + obj.size;
+	ctrl = ckteec_alloc_shm(ctrl_size, CKTEEC_SHM_INOUT);
+	if (!ctrl) {
+		rv = CKR_HOST_MEMORY;
+		goto bail;
+	}
+
+	buf = ctrl->buffer;
+
+	memcpy(buf, &session_handle, sizeof(session_handle));
+	buf += sizeof(session_handle);
+
+	memcpy(buf, &key_handle, sizeof(key_handle));
+	buf += sizeof(key_handle);
+
+	memcpy(buf, obj.buffer, obj.size);
+
+	rv = ckteec_invoke_ctrl(sign ? PKCS11_CMD_SIGN_INIT :
+				PKCS11_CMD_VERIFY_INIT, ctrl);
+
+bail:
+	ckteec_free_shm(ctrl);
+	release_serial_object(&obj);
+
+	return rv;
+}
+
+CK_RV ck_signverify_update(CK_SESSION_HANDLE session,
+			   CK_BYTE_PTR in,
+			   CK_ULONG in_len,
+			   int sign)
+{
+	CK_RV rv = CKR_GENERAL_ERROR;
+	TEEC_SharedMemory *ctrl = NULL;
+	TEEC_SharedMemory *in_shm = NULL;
+	uint32_t session_handle = session;
+
+	if (in_len && !in)
+		return CKR_ARGUMENTS_BAD;
+
+	/* Shm io0: (in/out) ctrl = [session-handle] / [status] */
+	ctrl = ckteec_alloc_shm(sizeof(session_handle), CKTEEC_SHM_INOUT);
+	if (!ctrl) {
+		rv = CKR_HOST_MEMORY;
+		goto bail;
+	}
+	memcpy(ctrl->buffer, &session_handle, sizeof(session_handle));
+
+	/* Shm io1: input data */
+	if (in_len) {
+		in_shm = ckteec_register_shm(in, in_len, CKTEEC_SHM_IN);
+		if (!in_shm) {
+			rv = CKR_HOST_MEMORY;
+			goto bail;
+		}
+	}
+
+	rv = ckteec_invoke_ctrl_in(sign ? PKCS11_CMD_SIGN_UPDATE :
+				   PKCS11_CMD_VERIFY_UPDATE, ctrl, in_shm);
+
+bail:
+	ckteec_free_shm(in_shm);
+	ckteec_free_shm(ctrl);
+
+	return rv;
+}
+
+CK_RV ck_signverify_oneshot(CK_SESSION_HANDLE session,
+			    CK_BYTE_PTR in,
+			    CK_ULONG in_len,
+			    CK_BYTE_PTR sign_ref,
+			    CK_ULONG_PTR sign_len,
+			    int sign)
+{
+	CK_RV rv = CKR_GENERAL_ERROR;
+	TEEC_SharedMemory *ctrl = NULL;
+	TEEC_SharedMemory *io1 = NULL;
+	TEEC_SharedMemory *io2 = NULL;
+	uint32_t session_handle = session;
+	size_t out_size = 0;
+
+	if ((in_len && !in) || (sign_len && *sign_len && !sign_ref) ||
+	    (sign && !sign_len))
+		return CKR_ARGUMENTS_BAD;
+
+	/* Shm io0: (in/out) ctrl = [session-handle] / [status] */
+	ctrl = ckteec_alloc_shm(sizeof(session_handle), CKTEEC_SHM_INOUT);
+	if (!ctrl) {
+		rv = CKR_HOST_MEMORY;
+		goto bail;
+	}
+	memcpy(ctrl->buffer, &session_handle, sizeof(session_handle));
+
+	/* Shm io1: input payload */
+	if (in_len) {
+		io1 = ckteec_register_shm(in, in_len, CKTEEC_SHM_IN);
+		if (!io1) {
+			rv = CKR_HOST_MEMORY;
+			goto bail;
+		}
+	}
+
+	/*
+	 * Shm io2: input signature (if verifying) or null sized shm
+	 * or
+	 * Shm io2: output signature (if signing) or null sized shm
+	 */
+	if (sign_len && *sign_len) {
+		io2 = ckteec_register_shm(sign_ref, *sign_len,
+					  sign ? CKTEEC_SHM_OUT : CKTEEC_SHM_IN);
+	} else {
+		/* Query size if output signature */
+		io2 = ckteec_alloc_shm(0, sign ? CKTEEC_SHM_OUT : CKTEEC_SHM_IN);
+	}
+
+	if (!io2) {
+		rv = CKR_HOST_MEMORY;
+		goto bail;
+	}
+
+	rv = ckteec_invoke_ta(sign ? PKCS11_CMD_SIGN_ONESHOT :
+			      PKCS11_CMD_VERIFY_ONESHOT, ctrl,
+			      io1, io2, &out_size, NULL, NULL);
+
+	if (sign && (rv == CKR_OK || rv == CKR_BUFFER_TOO_SMALL))
+		*sign_len = out_size;
+
+	if (rv == CKR_BUFFER_TOO_SMALL && out_size && !sign_ref)
+		rv = CKR_OK;
+
+bail:
+	ckteec_free_shm(io1);
+	ckteec_free_shm(io2);
+	ckteec_free_shm(ctrl);
+
+	return rv;
+}
+
+CK_RV ck_signverify_final(CK_SESSION_HANDLE session,
+			  CK_BYTE_PTR sign_ref,
+			  CK_ULONG_PTR sign_len,
+			  int sign)
+{
+	CK_RV rv = CKR_GENERAL_ERROR;
+	TEEC_SharedMemory *ctrl = NULL;
+	TEEC_SharedMemory *io = NULL;
+	uint32_t session_handle = session;
+	size_t out_size = 0;
+
+	if ((sign_len && *sign_len && !sign_ref) || (sign && !sign_len))
+		return CKR_ARGUMENTS_BAD;
+
+	/* Shm io0: (in/out) ctrl = [session-handle] / [status] */
+	ctrl = ckteec_alloc_shm(sizeof(session_handle), CKTEEC_SHM_INOUT);
+	if (!ctrl) {
+		rv = CKR_HOST_MEMORY;
+		goto bail;
+	}
+	memcpy(ctrl->buffer, &session_handle, sizeof(session_handle));
+
+	/*
+	 * Shm io1: input signature (if verifying) or null sized shm
+	 * or
+	 * Shm io1: output signature (if signing) or null sized shm
+	 */
+	if (sign_len && *sign_len)
+		io = ckteec_register_shm(sign_ref, *sign_len,
+					 sign ? CKTEEC_SHM_OUT : CKTEEC_SHM_IN);
+	else
+		io = ckteec_alloc_shm(0, sign ? CKTEEC_SHM_OUT : CKTEEC_SHM_IN);
+
+	if (!io) {
+		rv = CKR_HOST_MEMORY;
+		goto bail;
+	}
+
+	rv = ckteec_invoke_ctrl_out(sign ? PKCS11_CMD_SIGN_FINAL : PKCS11_CMD_VERIFY_FINAL,
+				    ctrl, io, &out_size);
+
+	if (sign && sign_len && (rv == CKR_OK || rv == CKR_BUFFER_TOO_SMALL))
+		*sign_len = out_size;
+
+	if (rv == CKR_BUFFER_TOO_SMALL && out_size && !sign_ref)
+		rv = CKR_OK;
+
+bail:
+	ckteec_free_shm(io);
+	ckteec_free_shm(ctrl);
+
+	return rv;
+}
+

libckteec/src/pkcs11_processing.h
@@ -36,4 +36,27 @@ CK_RV ck_encdecrypt_final(CK_SESSION_HANDLE session,
 			  CK_BYTE_PTR out,
 			  CK_ULONG_PTR out_len,
 			  int decrypt);
+
+CK_RV ck_signverify_init(CK_SESSION_HANDLE session,
+			 CK_MECHANISM_PTR mechanism,
+			 CK_OBJECT_HANDLE key,
+			 int sign);
+
+CK_RV ck_signverify_update(CK_SESSION_HANDLE session,
+			   CK_BYTE_PTR in,
+			   CK_ULONG in_len,
+			   int sign);
+
+CK_RV ck_signverify_oneshot(CK_SESSION_HANDLE session,
+			    CK_BYTE_PTR in,
+			    CK_ULONG in_len,
+			    CK_BYTE_PTR out,
+			    CK_ULONG_PTR out_len,
+			    int sign);
+
+CK_RV ck_signverify_final(CK_SESSION_HANDLE session,
+			  CK_BYTE_PTR out,
+			  CK_ULONG_PTR out_len,
+			  int sign);
+
 #endif /*LIBCKTEEC_PKCS11_PROCESSING_H*/

libckteec/src/serialize_ck.c
@@ -278,6 +278,12 @@ CK_RV serialize_ck_mecha_params(struct serializer *obj,
 	switch (mecha.mechanism) {
 	case CKM_AES_ECB:
 	case CKM_AES_CMAC:
+	case CKM_MD5_HMAC:
+	case CKM_SHA_1_HMAC:
+	case CKM_SHA224_HMAC:
+	case CKM_SHA256_HMAC:
+	case CKM_SHA384_HMAC:
+	case CKM_SHA512_HMAC:
 		/* No parameter expected, size shall be 0 */
 		if (mechanism->ulParameterLen)
 			return CKR_MECHANISM_PARAM_INVALID;

commit 4ede8089fc0d2c7fbfac986c97e4e7338c99514b

libckteec: fix spelling mistake
Fix spelling mistake in pkcs11_api.c.
Building in debug mode fails with the following error:

src/pkcs11_api.c: In function ‘C_Sign’:
src/pkcs11_api.c:969:36: error: ‘CKR_USER_NOT_LOGG_IN’ undeclared (first use in this function); did you mean ‘CKR_USER_NOT_LOGGED_IN’?
        CKR_SESSION_HANDLE_INVALID, CKR_USER_NOT_LOGG_IN,
                                    ^~~~~~~~~~~~~~~~~~~~

Fixes: 3f44b8702995 ("libckteec: add support for Signing and Verification Functions")
Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>
Signed-off-by: Pengguang Zhu <zpghao@163.com>

libckteec/src/pkcs11_api.c
@@ -966,7 +966,7 @@ CK_RV C_Sign(CK_SESSION_HANDLE hSession,
 		     CKR_DEVICE_REMOVED, CKR_FUNCTION_CANCELED,
 		     CKR_FUNCTION_FAILED, CKR_GENERAL_ERROR, CKR_HOST_MEMORY,
 		     CKR_OK, CKR_OPERATION_NOT_INITIALIZED, CKR_SESSION_CLOSED,
-		     CKR_SESSION_HANDLE_INVALID, CKR_USER_NOT_LOGG_IN,
+		     CKR_SESSION_HANDLE_INVALID, CKR_USER_NOT_LOGGED_IN,
 		     CKR_FUNCTION_REJECTED);
 
 	return rv;