OP-TEE/optee_client

commit 44c0d3056bbbeaa764a7406d14f6222a99bdef23

Makefile: preserve links to libraries during make copy_export
During "make copy_export" and "make install" the shared library is
copied into the LIBDIR. However "cp" is used, thus the links to the
shared library are not preserved. This patch fixes the problem by using
"cp -a" instead.

Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>
Acked-by: Jerome Forissier <jerome.forissier@linaro.org>

Makefile
@@ -125,6 +125,6 @@ distclean: clean
 
 copy_export: build
 	mkdir -p $(DESTDIR)$(BINDIR) $(DESTDIR)$(LIBDIR) $(DESTDIR)$(INCLUDEDIR)
-	cp ${O}/libteec/libteec.so* $(DESTDIR)$(LIBDIR)
+	cp -a ${O}/libteec/libteec.so* $(DESTDIR)$(LIBDIR)
 	cp ${O}/tee-supplicant/tee-supplicant $(DESTDIR)$(BINDIR)
 	cp public/*.h $(DESTDIR)$(INCLUDEDIR)

commit d66a452c15881958fc74e0bd6451604aa89ae6dd

Makefile: Makefile: only preserve links when installing output files
When installing output files with the `install` Makefile target, the
output file ownership is preserved because the file copy to the
destination directory is done with `cp -a/--archive`.

When using the `install` Makefile target in a Yocto recipe, it triggers
the following Bitbake host contamination warning:

WARNING: optee-client-3.17.0.imx-r0 do_package_qa: QA Issue: optee-client: /usr/lib/libseteec.so is owned by uid 1001, which is the same as the user running bitbake. This may be due to host contamination
optee-client: /usr/lib/libteec.so is owned by uid 1001, which is the same as the user running bitbake. This may be due to host contamination

The original goal of `cp -a` was to preserve symbolics links to shared
libraries. The issue is that it also preserves ownership which is
unwanted. To preserve symbolic links, `cp -d` is enough.

Fixes: 44c0d3056 ("Makefile: preserve links to libraries during make copy_export")
Signed-off-by: Clement Faure <clement.faure@nxp.com>
Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>

Makefile
@@ -137,13 +137,13 @@ distclean: clean
 copy_export: build
 	mkdir -p $(DESTDIR)$(SBINDIR) $(DESTDIR)$(LIBDIR) $(DESTDIR)$(INCLUDEDIR)
 	cp config.mk $(DESTDIR)/$(INCLUDEDIR)/optee_client_config.mk
-	cp -a ${O}/libteec/libteec.so* $(DESTDIR)$(LIBDIR)
-	cp -a ${O}/libteec/libteec.a $(DESTDIR)$(LIBDIR)
+	cp -d ${O}/libteec/libteec.so* $(DESTDIR)$(LIBDIR)
+	cp -d ${O}/libteec/libteec.a $(DESTDIR)$(LIBDIR)
 	cp ${O}/tee-supplicant/tee-supplicant $(DESTDIR)$(SBINDIR)
 	cp public/*.h $(DESTDIR)$(INCLUDEDIR)
 	cp libckteec/include/*.h $(DESTDIR)$(INCLUDEDIR)
-	cp -a ${O}/libckteec/libckteec.so* $(DESTDIR)$(LIBDIR)
-	cp -a ${O}/libckteec/libckteec.a $(DESTDIR)$(LIBDIR)
+	cp -d ${O}/libckteec/libckteec.so* $(DESTDIR)$(LIBDIR)
+	cp -d ${O}/libckteec/libckteec.a $(DESTDIR)$(LIBDIR)
 	cp libseteec/include/*.h $(DESTDIR)$(INCLUDEDIR)
-	cp -a ${O}/libseteec/libseteec.so* $(DESTDIR)$(LIBDIR)
-	cp -a ${O}/libseteec/libseteec.a $(DESTDIR)$(LIBDIR)
+	cp -d ${O}/libseteec/libseteec.so* $(DESTDIR)$(LIBDIR)
+	cp -d ${O}/libseteec/libseteec.a $(DESTDIR)$(LIBDIR)