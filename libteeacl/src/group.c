62c9ec7ec13b4202c30dc2ca52ed11efe06f4a1f
Eero Aaltonen <eero.aaltonen@vaisala.com>
2022-09-02 11:54:14 +0300

    Add libteeacl library
    This library will contain methods for configuring and using
    _Access Control List_ (ACL) authentication on tokens.
    
    Signed-off-by: Eero Aaltonen <eero.aaltonen@vaisala.com>
    Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>

---
+++ libteeacl/src/group.c
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (c) 2020, Vaisala Oyj.
+ */
+
+#include <teeacl.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+enum rv_groupmember teeacl_current_user_is_member_of(gid_t group)
+{
+	char username[L_cuserid] = { 0 };
+
+	cuserid(username);
+	return teeacl_user_is_member_of(username, group);
+}
+
+enum rv_groupmember teeacl_user_is_member_of(const char *user, gid_t group)
+{
+	enum rv_groupmember result = E_MEMORY;
+	int ret = 0;
+	int i = 0;
+	int grouplistsize = 8; /* initial guess */
+	gid_t *p_groups = NULL;
+	gid_t *groups = calloc(grouplistsize, sizeof(gid_t));
+	if (groups == NULL)
+		return E_MEMORY;
+	ret = getgrouplist(user, group, groups, &grouplistsize);
+
+	if (ret == -1) {
+		p_groups = groups;
+
+		groups = reallocarray(groups, grouplistsize, sizeof(gid_t));
+		if (groups == NULL) {
+			free(p_groups);
+			return E_MEMORY;
+		}
+		ret = getgrouplist(user, group, groups, &grouplistsize);
+		if (ret == -1) {
+			result = E_GROUPLIST;
+			goto out;
+		}
+	}
+
+	for (i = 0; i < grouplistsize; ++i) {
+		if (group == groups[i]) {
+			result = IS_MEMBER;
+			goto out;
+		}
+	}
+	result = NOT_MEMBER;
+out:
+	free(groups);
+	return result;
+}
