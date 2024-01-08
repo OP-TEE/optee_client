OP-TEE/optee_client

commit 3ac968ee7c927271e83ea3a4247839649202ab5e

Makefile, cmake: move teec related headers
The optee-client-headers interface library was added back in commit
e0a12e2322ae02e957910f29948bd19455bf9cfa "cmake: Initial CMake support"
, but to a directory named "public". Improve the consistency of the
Cmake build by moving the headers under the associated libteec include
directory.

Signed-off-by: Eero Aaltonen <eero.aaltonen@vaisala.com>
Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
Acked-by: Etienne Carriere <etienne.carriere@foss.st.com>

tee-supplicant/CMakeLists.txt
@@ -103,7 +103,6 @@ target_include_directories(${PROJECT_NAME} PRIVATE src)
 
 target_link_libraries(${PROJECT_NAME}
 	PRIVATE teec
-	PRIVATE optee-client-headers
 )
 
 if(CFG_TEE_SUPP_PLUGINS)

tee-supplicant/Makefile
@@ -40,7 +40,7 @@ TEES_OBJ_DIR	:= $(OUT_DIR)
 TEES_OBJS 	:= $(patsubst %.c,$(TEES_OBJ_DIR)/%.o, $(TEES_SRCS))
 TEES_INCLUDES 	:= ${CURDIR}/../libteec/include \
 		   ${CURDIR}/src \
-		   ${CURDIR}/../public \
+		   ${CURDIR}/../libteec/include \
 
 TEES_CFLAGS	:= $(addprefix -I, $(TEES_INCLUDES)) $(CFLAGS) \
 		   -DDEBUGLEVEL_$(CFG_TEE_SUPP_LOG_LEVEL) \

tee-supplicant/tee_supplicant_android.mk
@@ -62,7 +62,7 @@ ifeq ($(CFG_FTRACE_SUPPORT),y)
 LOCAL_CFLAGS += -DCFG_FTRACE_SUPPORT
 endif
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../public \
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../libteec/include \
                     $(LOCAL_PATH)/../libteec/include \
                     $(LOCAL_PATH)/src
 

CMakeLists.txt
@@ -40,7 +40,6 @@ endif(CCACHE_FOUND)
 
 add_subdirectory(libteec)
 add_subdirectory(tee-supplicant)
-add_subdirectory(public)
 add_subdirectory(libckteec)
 if(WITH_TEEACL)
     find_package(PkgConfig REQUIRED)

Makefile
@@ -160,7 +160,7 @@ copy_export: build
 	cp -d ${O}/libteec/libteec.so* $(DESTDIR)$(libdir)
 	cp -d ${O}/libteec/libteec.a $(DESTDIR)$(libdir)
 	cp ${O}/tee-supplicant/tee-supplicant $(DESTDIR)$(sbindir)
-	cp public/*.h $(DESTDIR)$(includedir)
+	cp libteec/include/*.h $(DESTDIR)$(includedir)
 	cp libckteec/include/*.h $(DESTDIR)$(includedir)
 	cp -d ${O}/libckteec/libckteec.so* $(DESTDIR)$(libdir)
 	cp -d ${O}/libckteec/libckteec.a $(DESTDIR)$(libdir)

libckteec/Makefile
@@ -31,7 +31,7 @@ LIBCKTEEC_SRCS		+= serializer.c
 LIBCKTEEC_SRCS		+= serialize_ck.c
 
 LIBCKTEEC_INCLUDES	= ${CURDIR}/include
-LIBCKTEEC_INCLUDES 	+= ${CURDIR}/../public
+LIBCKTEEC_INCLUDES 	+= ${CURDIR}/../libteec/include
 
 LIBCKTEEC_CFLAGS	:= $(addprefix -I, $(LIBCKTEEC_INCLUDES)) \
 			$(CFLAGS) -D_GNU_SOURCE -fPIC

libseteec/Makefile
@@ -24,7 +24,7 @@ LIBSETEEC_SRC_DIR	:= src
 LIBSETEEC_SRCS	= se.c
 
 LIBSETEEC_INCLUDES	= ${CURDIR}/include
-LIBSETEEC_INCLUDES 	+= ${CURDIR}/../public
+LIBSETEEC_INCLUDES 	+= ${CURDIR}/../libteec/include
 
 LIBSETEEC_CFLAGS	:= $(addprefix -I, $(LIBSETEEC_INCLUDES)) \
 			$(CFLAGS) -D_GNU_SOURCE -fPIC

libteec/CMakeLists.txt
@@ -63,11 +63,14 @@ endif()
 ################################################################################
 # Public and private header and library dependencies
 ################################################################################
-target_include_directories(teec PUBLIC include)
+target_include_directories(teec
+	PUBLIC include
+	PRIVATE src
+)
 
 target_link_libraries(teec
 	PUBLIC ${CMAKE_THREAD_LIBS_INIT} # this is pthreads
-	PRIVATE optee-client-headers)
+)
 
 ################################################################################
 # Install targets
@@ -76,3 +79,7 @@ target_link_libraries(teec
 # FIXME: Should we change this to /usr/local/lib?
 install(TARGETS teec LIBRARY DESTINATION "${CMAKE_INSTALL_LIBDIR}"
                       ARCHIVE DESTINATION "${CMAKE_INSTALL_LIBDIR}")
+
+install(DIRECTORY include
+	DESTINATION ${CMAKE_INSTALL_PREFIX}
+)

commit c410464dfb90e6346552620b5b7d7ebc58e1c5c4

tee-supplicant: fix build with kernel < 4.16
Commit 3ac968ee7c927271e83ea3a4247839649202ab5e moved linux/tee.h from
libteec/include to libteec/src resulting in the following build failure
with any kernel < 4.16 (i.e before
https://github.com/torvalds/linux/commit/033ddf12bcf5326b93bd604f50a7474a434a35f9):

/home/buildroot/autobuild/instance-3/output-1/build/optee-client-4.0.0/tee-supplicant/src/tee_supplicant.c: In function 'register_local_shm':
/home/buildroot/autobuild/instance-3/output-1/build/optee-client-4.0.0/tee-supplicant/src/tee_supplicant.c:356:44: error: storage size of 'data' isn't known
  356 |         struct tee_ioctl_shm_register_data data;
      |                                            ^~~~

To fix this build failure, update CMakeLists.txt and Makefile of
tee-supplicant to add libteec/src to the include directories.

Fixes: 3ac968ee7c92 ("Makefile, cmake: move teec related headers")

Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>
Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>

tee-supplicant/CMakeLists.txt
@@ -93,7 +93,10 @@ endif()
 ################################################################################
 # Public and private header and library dependencies
 ################################################################################
-target_include_directories(${PROJECT_NAME} PRIVATE src)
+target_include_directories(${PROJECT_NAME}
+	PRIVATE src
+	PRIVATE ../libteec/src
+)
 
 target_link_libraries(${PROJECT_NAME}
 	PRIVATE teec

tee-supplicant/Makefile
@@ -40,7 +40,7 @@ TEES_OBJ_DIR	:= $(OUT_DIR)
 TEES_OBJS 	:= $(patsubst %.c,$(TEES_OBJ_DIR)/%.o, $(TEES_SRCS))
 TEES_INCLUDES 	:= ${CURDIR}/../libteec/include \
 		   ${CURDIR}/src \
-		   ${CURDIR}/../libteec/include \
+		   ${CURDIR}/../libteec/src \
 
 TEES_CFLAGS	:= $(addprefix -I, $(TEES_INCLUDES)) $(CFLAGS) \
 		   -DDEBUGLEVEL_$(CFG_TEE_SUPP_LOG_LEVEL) \

tee-supplicant/tee_supplicant_android.mk
@@ -54,7 +54,7 @@ ifeq ($(CFG_FTRACE_SUPPORT),y)
 LOCAL_CFLAGS += -DCFG_FTRACE_SUPPORT
 endif
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../libteec/include \
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../libteec/src \
                     $(LOCAL_PATH)/../libteec/include \
                     $(LOCAL_PATH)/src
 