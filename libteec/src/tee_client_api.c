OP-TEE/optee_client

commit acb0885c117e73cb6c5c9b1dd9054cb3f93507ee

teec: use multiple of page size for page aligned buffers
When allocating a page aligned buffer, round the size up the next
multiple of page size. With this we can guarantee that a part of that
page isn't registered already.

Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Joakim Bech <joakim.bech@linaro.org>

libteec/src/tee_client_api.c
@@ -79,8 +79,10 @@ static void teec_mutex_unlock(pthread_mutex_t *mu)
 static void *teec_paged_aligned_alloc(size_t sz)
 {
 	void *p = NULL;
+	size_t page_sz = sysconf(_SC_PAGESIZE);
+	size_t aligned_sz = ((sz + page_sz - 1) / page_sz) * page_sz;
 
-	if (!posix_memalign(&p, sysconf(_SC_PAGESIZE), sz))
+	if (!posix_memalign(&p, page_sz, aligned_sz))
 		return p;
 
 	return NULL;

commit f412bac00ae038ba12177b1a60d90695ec405db6

teec: do fail on MAX_SIZE allocation requests
The variable aligned_sz will be 0 when the requested sz is MAX_SIZE.
Since posix_memalign can return a valid pointer for zero size
allocations, share memory registration requests for MAX_SIZE might make
it to the kernel.

This PR stops it early - just as it was before "teec: use multiple of
page size for page aligned buffers" was merged.

Fixes: acb0885c117e ("teec: use multiple of page size for page aligned buffers")
Signed-off-by: Jorge Ramirez-Ortiz <jorge@foundries.io>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

libteec/src/tee_client_api.c
@@ -82,7 +82,7 @@ static void *teec_paged_aligned_alloc(size_t sz)
 	size_t page_sz = sysconf(_SC_PAGESIZE);
 	size_t aligned_sz = ((sz + page_sz - 1) / page_sz) * page_sz;
 
-	if (!posix_memalign(&p, page_sz, aligned_sz))
+	if (aligned_sz >= sz && !posix_memalign(&p, page_sz, aligned_sz))
 		return p;
 
 	return NULL;