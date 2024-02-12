OP-TEE/optee_client

commit aa3dd58d605ed5588f459a2d4e67f4897467af6c

libckteec: Allow 0 length input buffer  for update operations.
If a valid input buffer with size 0 is passed in an update
operation like C_SignUpdate()/C_VerifyUpdate()/C_EncryptUpdate()/
C_DecryptUpdate(), current implementation return error
CKR_BAD_ARGUMENTS. This is actually not an error scenario.
Ideally, we should not perform any cryptographic operation
and return CKR_OK if other parameters are valid. Hence register
a shared memory buffer in such scenarios also and pass it to the TA.

Suggested-by: Etienne Carriere <etienne.carriere@linaro.org>
Signed-off-by: Ruchika Gupta <ruchika.gupta@linaro.org>
Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

libckteec/src/pkcs11_processing.c
@@ -252,12 +252,10 @@ CK_RV ck_encdecrypt_oneshot(CK_SESSION_HANDLE session,
 	memcpy(ctrl->buffer, &session_handle, sizeof(session_handle));
 
 	/* Shm io1: input data buffer */
-	if (in_len) {
-		in_shm = ckteec_register_shm(in, in_len, CKTEEC_SHM_IN);
-		if (!in_shm) {
-			rv = CKR_HOST_MEMORY;
-			goto bail;
-		}
+	in_shm = ckteec_register_shm(in, in_len, CKTEEC_SHM_IN);
+	if (!in_shm) {
+		rv = CKR_HOST_MEMORY;
+		goto bail;
 	}
 
 	/* Shm io2: output data buffer */
@@ -417,12 +415,10 @@ CK_RV ck_signverify_update(CK_SESSION_HANDLE session,
 	memcpy(ctrl->buffer, &session_handle, sizeof(session_handle));
 
 	/* Shm io1: input data */
-	if (in_len) {
-		in_shm = ckteec_register_shm(in, in_len, CKTEEC_SHM_IN);
-		if (!in_shm) {
-			rv = CKR_HOST_MEMORY;
-			goto bail;
-		}
+	in_shm = ckteec_register_shm(in, in_len, CKTEEC_SHM_IN);
+	if (!in_shm) {
+		rv = CKR_HOST_MEMORY;
+		goto bail;
 	}
 
 	rv = ckteec_invoke_ctrl_in(sign ? PKCS11_CMD_SIGN_UPDATE :

commit 0e0fd2111c41d3235afa057a3ff5b5b07c85c79d

libckteec: one shot encryption/decryption may have no input data
Change the one shot encryption and decryption handler function to
allow input buffer reference to be NULL. This may happen for example
with AES GCM operation where encryption of a NULL buffer is expected to
produce an AES GCM authentication tag. Before this change, providing a
NULL buffer to C_Encrypt() made ckteec_register_shm() to fail and
ckteec_register_shm() to return CKR_HOST_MEMORY error code.

Fixes: aa3dd58d605e ("libckteec: Allow 0 length input buffer  for update operations.")
Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>

libckteec/src/pkcs11_processing.c
@@ -262,10 +262,12 @@ CK_RV ck_encdecrypt_oneshot(CK_SESSION_HANDLE session,
 	memcpy(ctrl->buffer, &session_handle, sizeof(session_handle));
 
 	/* Shm io1: input data buffer */
-	in_shm = ckteec_register_shm(in, in_len, CKTEEC_SHM_IN);
-	if (!in_shm) {
-		rv = CKR_HOST_MEMORY;
-		goto bail;
+	if (in) {
+		in_shm = ckteec_register_shm(in, in_len, CKTEEC_SHM_IN);
+		if (!in_shm) {
+			rv = CKR_HOST_MEMORY;
+			goto bail;
+		}
 	}
 
 	/* Shm io2: output data buffer */