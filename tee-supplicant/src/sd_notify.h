e3ebccc0dbd093fcd2329990277ce3b58aafbd23
Ayoub Zaki <ayoub.zaki@embetrix.com>
2024-05-30 13:36:59 +0200

    tee-supplicant: send READY=1 notification to systemd
    This option is very useful when tee-supplicant is started
    from systemd and can used with Type=notify to signal readiness
    
    Signed-off-by: Ayoub Zaki <ayoub.zaki@embetrix.com>
    Acked-by: Jerome Forissier <jerome.forissier@linaro.org>

---
+++ tee-supplicant/src/sd_notify.h
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT-0
+/*
+ * The code below is imported from:
+ * https://www.freedesktop.org/software/systemd/man/devel/sd_notify.html#Standalone%20Implementations
+ */
+#ifndef SD_NOTIFY_H
+#define SD_NOTIFY_H
+
+int sd_notify_ready(void);
+
+#endif /* SD_NOTIFY_H */
