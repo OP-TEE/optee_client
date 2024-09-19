OP-TEE/optee_client

commit e88c264ba358941d2166b18eeac448e617ffa887

libckteec: helper function to serialize a attribute template
Helper function serialize_ck_attributes() takes an attribute list
and generates a serial representation of it.

The template contains item that can be of a given size or that can
be a list of template, referred to as indirect attributes.

In the process, the Cryptoki ULONG data are serialized as 32bit
data as per PKCS11 TA ABI.

Acked-by: Jerome Forissier <jerome@forissier.org>
Co-developed-by: Etienne Carriere <etienne.carriere@linaro.org>
Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

libckteec/src/serialize_ck.c
@@ -1,0 +1,194 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (c) 2017-2020, Linaro Limited
+ */
+
+#include <ck_debug.h>
+#include <inttypes.h>
+#include <pkcs11.h>
+#include <pkcs11_ta.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "ck_helpers.h"
+#include "local_utils.h"
+#include "serializer.h"
+#include "serialize_ck.h"
+
+/*
+ * Generic way of serializing CK keys, certificates, mechanism parameters, ...
+ * In cryptoki 2.40 parameters are almost all packaged as structure below:
+ */
+struct ck_ref {
+	CK_ULONG id;
+	CK_BYTE_PTR ptr;
+	CK_ULONG len;
+};
+
+/*
+ * This is for attributes that contains data memory indirections.
+ * In other words, an attributes that defines a list of attributes.
+ * They are identified from the attribute type CKA_...
+ *
+ * @obj - ref used to track the serial object being created
+ * @attribute - pointer to a structure aligned of the CK_ATTRIBUTE struct
+ */
+static CK_RV serialize_indirect_attribute(struct serializer *obj,
+					  CK_ATTRIBUTE_PTR attribute)
+{
+	CK_ATTRIBUTE_PTR attr = NULL;
+	CK_ULONG count = 0;
+	CK_RV rv = CKR_GENERAL_ERROR;
+	struct serializer obj2 = { };
+
+	switch (attribute->type) {
+	/* These are serialized each separately */
+	case CKA_WRAP_TEMPLATE:
+	case CKA_UNWRAP_TEMPLATE:
+		count = attribute->ulValueLen / sizeof(CK_ATTRIBUTE);
+		attr = (CK_ATTRIBUTE_PTR)attribute->pValue;
+		break;
+	default:
+		return CKR_NO_EVENT;
+	}
+
+	/* Create a serialized object for the content */
+	rv = serialize_ck_attributes(&obj2, attr, count);
+	if (rv)
+		return rv;
+
+	/*
+	 * Append the created serialized object into target object:
+	 * [attrib-id][byte-size][attributes-data]
+	 */
+	rv = serialize_32b(obj, attribute->type);
+	if (rv)
+		return rv;
+
+	rv = serialize_32b(obj, obj2.size);
+	if (rv)
+		return rv;
+
+	rv = serialize_buffer(obj, obj2.buffer, obj2.size);
+	if (rv)
+		return rv;
+
+	obj->item_count++;
+
+	return rv;
+}
+
+static int ck_attr_is_ulong(CK_ATTRIBUTE_TYPE attribute_id)
+{
+	switch (attribute_id) {
+	case CKA_CLASS:
+	case CKA_CERTIFICATE_TYPE:
+	case CKA_KEY_TYPE:
+	case CKA_HW_FEATURE_TYPE:
+	case CKA_MECHANISM_TYPE:
+	case CKA_KEY_GEN_MECHANISM:
+	case CKA_VALUE_LEN:
+	case CKA_MODULUS_BITS:
+		return true;
+	default:
+		return false;
+	}
+}
+
+static CK_RV serialize_ck_attribute(struct serializer *obj, CK_ATTRIBUTE *attr)
+{
+	CK_MECHANISM_TYPE *type = NULL;
+	uint32_t pkcs11_size = 0;
+	uint32_t pkcs11_data32 = 0;
+	void *pkcs11_pdata = NULL;
+	uint32_t *mech_buf = NULL;
+	CK_RV rv = CKR_GENERAL_ERROR;
+	unsigned int n = 0;
+	unsigned int m = 0;
+
+	if (attr->type == PKCS11_UNDEFINED_ID)
+		return CKR_ATTRIBUTE_TYPE_INVALID;
+
+	switch (attr->type) {
+	case CKA_WRAP_TEMPLATE:
+	case CKA_UNWRAP_TEMPLATE:
+		return serialize_indirect_attribute(obj, attr);
+	case CKA_ALLOWED_MECHANISMS:
+		n = attr->ulValueLen / sizeof(CK_ULONG);
+		pkcs11_size = n * sizeof(uint32_t);
+		mech_buf = malloc(pkcs11_size);
+		if (!mech_buf)
+			return CKR_HOST_MEMORY;
+
+		type = attr->pValue;
+		for (m = 0; m < n; m++) {
+			mech_buf[m] = type[m];
+			if (mech_buf[m] == PKCS11_UNDEFINED_ID) {
+				rv = CKR_MECHANISM_INVALID;
+				goto out;
+			}
+		}
+		pkcs11_pdata = mech_buf;
+		break;
+	/* Attributes which data value do not need conversion (aside ulong) */
+	default:
+		if (ck_attr_is_ulong(attr->type)) {
+			CK_ULONG ck_ulong = 0;
+
+			if (attr->ulValueLen != sizeof(CK_ULONG))
+				return CKR_ATTRIBUTE_TYPE_INVALID;
+
+			memcpy(&ck_ulong, attr->pValue, sizeof(ck_ulong));
+			pkcs11_data32 = ck_ulong;
+			pkcs11_pdata = &pkcs11_data32;
+			pkcs11_size = sizeof(uint32_t);
+		} else {
+			pkcs11_pdata = attr->pValue;
+			pkcs11_size = attr->ulValueLen;
+		}
+		break;
+	}
+
+	rv = serialize_32b(obj, attr->type);
+	if (rv)
+		goto out;
+
+	rv = serialize_32b(obj, pkcs11_size);
+	if (rv)
+		goto out;
+
+	rv = serialize_buffer(obj, pkcs11_pdata, pkcs11_size);
+	if (rv)
+		goto out;
+
+	obj->item_count++;
+out:
+	free(mech_buf);
+
+	return rv;
+}
+
+/* CK attribute reference arguments are list of attribute item */
+CK_RV serialize_ck_attributes(struct serializer *obj,
+			      CK_ATTRIBUTE_PTR attributes, CK_ULONG count)
+{
+	CK_ULONG n = 0;
+	CK_RV rv = CKR_OK;
+
+	rv = init_serial_object(obj);
+	if (rv)
+		return rv;
+
+	for (n = 0; n < count; n++) {
+		rv = serialize_ck_attribute(obj, attributes + n);
+		if (rv)
+			break;
+	}
+
+	if (rv)
+		release_serial_object(obj);
+	else
+		finalize_serial_object(obj);
+
+	return rv;
+}

libckteec/CMakeLists.txt
@@ -26,6 +26,7 @@ set (SRC
 	src/invoke_ta.c
 	src/pkcs11_token.c
 	src/serializer.c
+	src/serialize_ck.c
 )
 
 ################################################################################

libckteec/Makefile
@@ -27,6 +27,7 @@ LIBCKTEEC_SRCS		+= ck_helpers.c
 LIBCKTEEC_SRCS		+= invoke_ta.c
 LIBCKTEEC_SRCS		+= pkcs11_token.c
 LIBCKTEEC_SRCS		+= serializer.c
+LIBCKTEEC_SRCS		+= serialize_ck.c
 
 LIBCKTEEC_INCLUDES	= ${CURDIR}/include
 LIBCKTEEC_INCLUDES 	+= ${CURDIR}/../public

libckteec/src/serialize_ck.h
@@ -1,0 +1,16 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (c) 2017-2020, Linaro Limited
+ */
+#ifndef LIBCKTEEC_SERIALIZE_CK_H
+#define LIBCKTEEC_SERIALIZE_CK_H
+
+#include <pkcs11.h>
+
+#include "serializer.h"
+
+/* Create (and allocate) a serial object for CK_ATTRIBUTE array */
+CK_RV serialize_ck_attributes(struct serializer *obj,
+			      CK_ATTRIBUTE_PTR attributes, CK_ULONG count);
+
+#endif /*LIBCKTEEC_SERIALIZE_CK_H*/

commit dc3dc433f36df5745bf99be94d8df7abc53c1ed0

libckteec: fix memory allocation leakage on template serialization
Fix memory allocation leakage with a call to release_serial_object()
to release obj2 before serialize_indirect_attribute() returns.

Link: https://github.com/OP-TEE/optee_client/issues/387
Fixes: e88c264ba358 ("libckteec: helper function to serialize a attribute template")
Signed-off-by: Hoyong Jin <hoyong2007@naver.com>
Reviewed-by: Etienne Carriere <etienne.carriere@foss.st.com>

libckteec/src/serialize_ck.c
@@ -95,17 +95,19 @@ static CK_RV serialize_indirect_attribute(struct serializer *obj,
 	 */
 	rv = serialize_32b(obj, attribute->type);
 	if (rv)
-		return rv;
+		goto out;
 
 	rv = serialize_32b(obj, obj2.size);
 	if (rv)
-		return rv;
+		goto out;
 
 	rv = serialize_buffer(obj, obj2.buffer, obj2.size);
 	if (rv)
-		return rv;
+		goto out;
 
 	obj->item_count++;
+out:
+	release_serial_object(&obj2);
 
 	return rv;
 }