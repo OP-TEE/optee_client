OP-TEE/optee_client

commit 2c812c5b4f81a97687fb42196c1225545cbeb7e8

libckteec: return CKR_CANT_LOCK if mutex fails at lib initialization
Return CKR_CANT_LOCK if mutex fails at lib initialization. This
conforms with the PKCS#11 specification v2.40-errata01. Preserve
the exit cases on mutex issues when unlocking and in C_Finalize().

Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
Acked-by: Ruchika Gupta <ruchika.gupta@linaro.org>

libckteec/src/invoke_ta.c
@@ -283,11 +283,8 @@ CK_RV ckteec_invoke_init(void)
 	}
 
 	e = pthread_mutex_lock(&ta_ctx.init_mutex);
-	if (e) {
-		EMSG("pthread_mutex_lock: %s", strerror(e));
-		EMSG("terminating...");
-		exit(EXIT_FAILURE);
-	}
+	if (e)
+		return CKR_CANT_LOCK;
 
 	if (ta_ctx.initiated) {
 		rv = CKR_CRYPTOKI_ALREADY_INITIALIZED;

commit 877d082672970cb542576fb3a9c44abb87e403af

libckteec: replace strerror with strerror_r in invoke_ta.c
strerror() returns a pointer to a shared static buffer that is not
thread-safe. Since libckteec uses pthread_mutex for session
management, concurrent threads hitting mutex errors could observe
corrupted error strings.

Replace with strerror_r() which writes to a caller-provided buffer.
Check the return value and null-terminate on failure.

Fixes: 2c812c5 ("libckteec: return CKR_CANT_LOCK if mutex fails at lib initialization")
Reported-by: Tobi Gaertner <tggaertn@amazon.com>
Signed-off-by: Georges Savoundararadj <savoundg@amazon.com>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

libckteec/src/invoke_ta.c
@@ -15,6 +15,8 @@
 #include <pthread.h>
 #include <stdlib.h>
 #include <string.h>
+
+#define STRERROR_BUF_SIZE 128
 #include <sys/types.h>
 #include <tee_client_api.h>
 #include <teec_trace.h>
@@ -319,7 +321,11 @@ CK_RV ckteec_invoke_init(void)
 out:
 	e = pthread_mutex_unlock(&ta_ctx.init_mutex);
 	if (e) {
-		EMSG("pthread_mutex_unlock: %s", strerror(e));
+		char errbuf[STRERROR_BUF_SIZE];
+
+		if (strerror_r(e, errbuf, sizeof(errbuf)))
+			errbuf[0] = '\0';
+		EMSG("pthread_mutex_unlock: %s", errbuf);
 		EMSG("terminating...");
 		exit(EXIT_FAILURE);
 	}
@@ -334,7 +340,11 @@ CK_RV ckteec_invoke_terminate(void)
 
 	e = pthread_mutex_lock(&ta_ctx.init_mutex);
 	if (e) {
-		EMSG("pthread_mutex_lock: %s", strerror(e));
+		char errbuf[STRERROR_BUF_SIZE];
+
+		if (strerror_r(e, errbuf, sizeof(errbuf)))
+			errbuf[0] = '\0';
+		EMSG("pthread_mutex_lock: %s", errbuf);
 		EMSG("terminating...");
 		exit(EXIT_FAILURE);
 	}
@@ -351,7 +361,11 @@ CK_RV ckteec_invoke_terminate(void)
 out:
 	e = pthread_mutex_unlock(&ta_ctx.init_mutex);
 	if (e) {
-		EMSG("pthread_mutex_unlock: %s", strerror(e));
+		char errbuf[STRERROR_BUF_SIZE];
+
+		if (strerror_r(e, errbuf, sizeof(errbuf)))
+			errbuf[0] = '\0';
+		EMSG("pthread_mutex_unlock: %s", errbuf);
 		EMSG("terminating...");
 		exit(EXIT_FAILURE);
 	}