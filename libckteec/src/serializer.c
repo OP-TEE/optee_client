OP-TEE/optee_client

commit 85a7ea7861339356b39c73926f2b5cb9added45b

libckteec: introduce helpers for serializing data
New helper functions to serialize data provided by client into
attribute list structure with a header and followed by the
serialized item. Each serialized item is made of a item ID,
an item value size followed by the item binary value as a byte
array.

init_serial_object() initializes a serial object.

finalize_serial_object() sets attributes meta data (byte size,
item count) in attribute list header.

release_serial_object() frees resources allocated by serialization
process.

serialize_buffer() appends an item of variable size value.
serialize_32b() appends a item of 32bit size value.

Acked-by: Jerome Forissier <jerome@forissier.org>
Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

libckteec/src/serializer.c
@@ -1,0 +1,69 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (c) 2017-2020, Linaro Limited
+ */
+
+#include <pkcs11_ta.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "ck_helpers.h"
+#include "local_utils.h"
+#include "serializer.h"
+
+CK_RV init_serial_object(struct serializer *obj)
+{
+	struct pkcs11_object_head head = { };
+
+	memset(obj, 0, sizeof(*obj));
+
+	return serialize_buffer(obj, &head, sizeof(head));
+}
+
+void finalize_serial_object(struct serializer *obj)
+{
+	struct pkcs11_object_head head = { };
+
+	head.attrs_size = obj->size - sizeof(head);
+	head.attrs_count = obj->item_count;
+	memcpy(obj->buffer, &head, sizeof(head));
+}
+
+void release_serial_object(struct serializer *obj)
+{
+	free(obj->buffer);
+	obj->buffer = NULL;
+}
+
+/**
+ * serialize - append data in a serialized buffer
+ *
+ * Serialize data in provided buffer.
+ * Ensure 64byte alignment of appended data in the buffer.
+ */
+static CK_RV serialize(char **bstart, size_t *blen, void *data, size_t len)
+{
+	size_t nlen = *blen + len;
+	char *buf = realloc(*bstart, nlen);
+
+	if (!buf)
+		return CKR_HOST_MEMORY;
+
+	memcpy(buf + *blen, data, len);
+
+	*blen = nlen;
+	*bstart = buf;
+
+	return CKR_OK;
+}
+
+CK_RV serialize_buffer(struct serializer *obj, void *data, size_t size)
+{
+	return serialize(&obj->buffer, &obj->size, data, size);
+}
+
+CK_RV serialize_32b(struct serializer *obj, uint32_t data)
+{
+	return serialize_buffer(obj, &data, sizeof(data));
+}

libckteec/CMakeLists.txt
@@ -25,6 +25,7 @@ set (SRC
 	src/ck_helpers.c
 	src/invoke_ta.c
 	src/pkcs11_token.c
+	src/serializer.c
 )
 
 ################################################################################

libckteec/Makefile
@@ -26,6 +26,7 @@ LIBCKTEEC_SRCS		+= ck_debug.c
 LIBCKTEEC_SRCS		+= ck_helpers.c
 LIBCKTEEC_SRCS		+= invoke_ta.c
 LIBCKTEEC_SRCS		+= pkcs11_token.c
+LIBCKTEEC_SRCS		+= serializer.c
 
 LIBCKTEEC_INCLUDES	= ${CURDIR}/include
 LIBCKTEEC_INCLUDES 	+= ${CURDIR}/../public

libckteec/src/serializer.h
@@ -1,0 +1,31 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (c) 2017-2020, Linaro Limited
+ */
+#ifndef LIBCKTEEC_SERIALIZER_H
+#define LIBCKTEEC_SERIALIZER_H
+
+#include <pkcs11.h>
+#include <pkcs11_ta.h>
+#include <stddef.h>
+#include <stdint.h>
+
+/*
+ * Struct used to create the buffer storing the serialized data.
+ * Contains some fields to help parsing content (type/boolprops).
+ */
+struct serializer {
+	char *buffer;		/* serial buffer base address */
+	size_t size;		/* serial buffer current byte size */
+	size_t item_count;	/* number of items in entry table */
+};
+
+/* Init/finalize/release a serializer object */
+CK_RV init_serial_object(struct serializer *obj);
+void finalize_serial_object(struct serializer *obj);
+void release_serial_object(struct serializer *obj);
+
+CK_RV serialize_buffer(struct serializer *obj, void *data, size_t size);
+CK_RV serialize_32b(struct serializer *obj, uint32_t data);
+
+#endif /*LIBCKTEEC_SERIALIZER_H*/

commit 0ceb997e29f596af25e8aa049c3fa5365a521b15

libckteec: add data pointer validation in serialize()
serialize() calls memcpy(buf + *blen, data, len) without validating
the data pointer. When a caller passes a wild or NULL pointer with a
non-zero len, this causes an invalid memory access (CWE-476).

Add input validation:
- Reject len > 0 with NULL data, returning CKR_ARGUMENTS_BAD
- Short-circuit len == 0 as a no-op, returning CKR_OK
- Move variable declarations to top for C89 compliance

Found by fuzzing with AFL++/AddressSanitizer.

Fixes: 85a7ea7 ("libckteec: introduce helpers for serializing data")
Reported-by: Tobi Gaertner <tggaertn@amazon.com>
Signed-off-by: Georges Savoundararadj <savoundg@amazon.com>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

libckteec/src/serializer.c
@@ -5,6 +5,7 @@
 
 #include <pkcs11_ta.h>
 #include <stddef.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -44,8 +45,20 @@ void release_serial_object(struct serializer *obj)
  */
 static CK_RV serialize(char **bstart, size_t *blen, void *data, size_t len)
 {
-	size_t nlen = *blen + len;
-	char *buf = realloc(*bstart, nlen);
+	size_t nlen = 0;
+	char *buf = NULL;
+
+	if (len && !data)
+		return CKR_ARGUMENTS_BAD;
+
+	if (!len)
+		return CKR_OK;
+
+	if (len > SIZE_MAX - *blen)
+		return CKR_DEVICE_MEMORY;
+
+	nlen = *blen + len;
+	buf = realloc(*bstart, nlen);
 
 	if (!buf)
 		return CKR_HOST_MEMORY;