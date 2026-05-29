OP-TEE/optee_client

commit e7a4706e08c8fbdd53530868a6ac4937193dd73c

libckteec: add support for symmetric ciphers
Adds support for symmetric encryption and decryption by implementing:
- C_EncryptInit()
- C_Encrypt()
- C_EncryptUpdate()
- C_EncryptFinal()
- C_DecryptInit()
- C_Decrypt()
- C_DecryptUpdate()
- C_DecryptFinal()

Reviewed-by: Vesa Jääskeläinen <vesa.jaaskelainen@vaisala.com>
Co-developed-by: Etienne Carriere <etienne.carriere@linaro.org>
Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

libckteec/src/serialize_ck.c
@@ -192,3 +192,111 @@ CK_RV serialize_ck_attributes(struct serializer *obj,
 
 	return rv;
 }
+
+/*
+ * Serialization of CK mechanism parameters
+ *
+ * Most mechanism have no parameters.
+ * Some mechanism have a single 32bit parameter.
+ * Some mechanism have a specific parameter structure which may contain
+ * indirected data (data referred by a buffer pointer).
+ *
+ * Below are each structure specific mechanisms parameters.
+ */
+
+static CK_RV serialize_mecha_aes_ctr(struct serializer *obj,
+				     CK_MECHANISM_PTR mecha)
+{
+	CK_AES_CTR_PARAMS_PTR param = mecha->pParameter;
+	CK_RV rv = CKR_GENERAL_ERROR;
+	uint32_t size = 0;
+
+	rv = serialize_32b(obj, obj->type);
+	if (rv)
+		return rv;
+
+	size = sizeof(uint32_t) + sizeof(param->cb);
+	rv = serialize_32b(obj, size);
+	if (rv)
+		return rv;
+
+	rv = serialize_ck_ulong(obj, param->ulCounterBits);
+	if (rv)
+		return rv;
+
+	rv = serialize_buffer(obj, param->cb, sizeof(param->cb));
+	if (rv)
+		return rv;
+
+	return rv;
+}
+
+static CK_RV serialize_mecha_aes_iv(struct serializer *obj,
+				    CK_MECHANISM_PTR mecha)
+{
+	uint32_t iv_size = mecha->ulParameterLen;
+	CK_RV rv = CKR_GENERAL_ERROR;
+
+	rv = serialize_32b(obj, obj->type);
+	if (rv)
+		return rv;
+
+	rv = serialize_32b(obj, iv_size);
+	if (rv)
+		return rv;
+
+	return serialize_buffer(obj, mecha->pParameter, mecha->ulParameterLen);
+}
+
+/**
+ * serialize_ck_mecha_params - serialize a mechanism type & params
+ *
+ * @obj - serializer used to track the serialization
+ * @mechanism - pointer of the in structure aligned CK_MECHANISM.
+ *
+ * Serialized content:
+ *	[mechanism-type][mechanism-param-blob]
+ *
+ * [mechanism-param-blob] depends on mechanism type ID, see
+ * serialize_mecha_XXX().
+ */
+CK_RV serialize_ck_mecha_params(struct serializer *obj,
+				CK_MECHANISM_PTR mechanism)
+{
+	CK_MECHANISM mecha = { };
+	CK_RV rv = CKR_GENERAL_ERROR;
+
+	memset(obj, 0, sizeof(*obj));
+
+	obj->object = PKCS11_CKO_MECHANISM;
+
+	mecha = *mechanism;
+	obj->type = mecha.mechanism;
+	if (obj->type == PKCS11_UNDEFINED_ID)
+		return CKR_MECHANISM_INVALID;
+
+	switch (mecha.mechanism) {
+	case CKM_AES_ECB:
+	case CKM_AES_CMAC:
+		/* No parameter expected, size shall be 0 */
+		if (mechanism->ulParameterLen)
+			return CKR_MECHANISM_PARAM_INVALID;
+
+		rv = serialize_32b(obj, obj->type);
+		if (rv)
+			return rv;
+
+		return serialize_32b(obj, 0);
+
+	case CKM_AES_CBC:
+	case CKM_AES_CBC_PAD:
+	case CKM_AES_CTS:
+		return serialize_mecha_aes_iv(obj, &mecha);
+
+	case CKM_AES_CTR:
+		return serialize_mecha_aes_ctr(obj, &mecha);
+
+	default:
+		return CKR_MECHANISM_INVALID;
+	}
+}

libckteec/src/pkcs11_api.c
@@ -602,14 +602,23 @@ CK_RV C_EncryptInit(CK_SESSION_HANDLE hSession,
 		    CK_MECHANISM_PTR pMechanism,
 		    CK_OBJECT_HANDLE hKey)
 {
-	(void)hSession;
-	(void)pMechanism;
-	(void)hKey;
+	CK_RV rv = CKR_CRYPTOKI_NOT_INITIALIZED;
 
-	if (!lib_initiated())
-		return CKR_CRYPTOKI_NOT_INITIALIZED;
+	if (lib_initiated())
+		rv = ck_encdecrypt_init(hSession, pMechanism, hKey, CK_FALSE);
 
-	return CKR_FUNCTION_NOT_SUPPORTED;
+	ASSERT_CK_RV(rv, CKR_CRYPTOKI_NOT_INITIALIZED, CKR_DEVICE_ERROR,
+		     CKR_DEVICE_MEMORY, CKR_DEVICE_REMOVED,
+		     CKR_FUNCTION_CANCELED, CKR_FUNCTION_FAILED,
+		     CKR_GENERAL_ERROR, CKR_HOST_MEMORY,
+		     CKR_KEY_FUNCTION_NOT_PERMITTED, CKR_KEY_HANDLE_INVALID,
+		     CKR_KEY_SIZE_RANGE, CKR_KEY_TYPE_INCONSISTENT,
+		     CKR_MECHANISM_INVALID, CKR_MECHANISM_PARAM_INVALID,
+		     CKR_OK, CKR_OPERATION_ACTIVE, CKR_PIN_EXPIRED,
+		     CKR_SESSION_CLOSED, CKR_SESSION_HANDLE_INVALID,
+		     CKR_USER_NOT_LOGGED_IN);
+
+	return rv;
 }
 
 CK_RV C_Encrypt(CK_SESSION_HANDLE hSession,
@@ -618,16 +627,22 @@ CK_RV C_Encrypt(CK_SESSION_HANDLE hSession,
 		CK_BYTE_PTR pEncryptedData,
 		CK_ULONG_PTR pulEncryptedDataLen)
 {
-	(void)hSession;
-	(void)pData;
-	(void)ulDataLen;
-	(void)pEncryptedData;
-	(void)pulEncryptedDataLen;
+	CK_RV rv = CKR_CRYPTOKI_NOT_INITIALIZED;
 
-	if (!lib_initiated())
-		return CKR_CRYPTOKI_NOT_INITIALIZED;
+	if (lib_initiated())
+		rv = ck_encdecrypt_oneshot(hSession, pData, ulDataLen,
+					   pEncryptedData, pulEncryptedDataLen,
+					   CK_FALSE);
 
-	return CKR_FUNCTION_NOT_SUPPORTED;
+	ASSERT_CK_RV(rv, CKR_ARGUMENTS_BAD, CKR_BUFFER_TOO_SMALL,
+		     CKR_CRYPTOKI_NOT_INITIALIZED, CKR_DATA_LEN_RANGE,
+		     CKR_DEVICE_ERROR, CKR_DEVICE_MEMORY, CKR_DEVICE_REMOVED,
+		     CKR_FUNCTION_CANCELED, CKR_FUNCTION_FAILED,
+		     CKR_GENERAL_ERROR, CKR_HOST_MEMORY, CKR_OK,
+		     CKR_OPERATION_NOT_INITIALIZED, CKR_SESSION_CLOSED,
+		     CKR_SESSION_HANDLE_INVALID);
+
+	return rv;
 }
 
 CK_RV C_EncryptUpdate(CK_SESSION_HANDLE hSession,
@@ -636,44 +651,66 @@ CK_RV C_EncryptUpdate(CK_SESSION_HANDLE hSession,
 		      CK_BYTE_PTR pEncryptedData,
 		      CK_ULONG_PTR pulEncryptedDataLen)
 {
-	(void)hSession;
-	(void)pPart;
-	(void)ulPartLen;
-	(void)pEncryptedData;
-	(void)pulEncryptedDataLen;
+	CK_RV rv = CKR_CRYPTOKI_NOT_INITIALIZED;
 
-	if (!lib_initiated())
-		return CKR_CRYPTOKI_NOT_INITIALIZED;
+	if (lib_initiated())
+		rv = ck_encdecrypt_update(hSession, pPart, ulPartLen,
+					  pEncryptedData,
+					  pulEncryptedDataLen, CK_FALSE);
 
-	return CKR_FUNCTION_NOT_SUPPORTED;
+	ASSERT_CK_RV(rv, CKR_ARGUMENTS_BAD, CKR_BUFFER_TOO_SMALL,
+		     CKR_CRYPTOKI_NOT_INITIALIZED, CKR_DATA_LEN_RANGE,
+		     CKR_DEVICE_ERROR, CKR_DEVICE_MEMORY, CKR_DEVICE_REMOVED,
+		     CKR_FUNCTION_CANCELED, CKR_FUNCTION_FAILED,
+		     CKR_GENERAL_ERROR, CKR_HOST_MEMORY, CKR_OK,
+		     CKR_OPERATION_NOT_INITIALIZED, CKR_SESSION_CLOSED,
+		     CKR_SESSION_HANDLE_INVALID);
+
+	return rv;
 }
 
 CK_RV C_EncryptFinal(CK_SESSION_HANDLE hSession,
 		     CK_BYTE_PTR pLastEncryptedPart,
 		     CK_ULONG_PTR pulLastEncryptedPartLen)
 {
-	(void)hSession;
-	(void)pLastEncryptedPart;
-	(void)pulLastEncryptedPartLen;
+	CK_RV rv = CKR_CRYPTOKI_NOT_INITIALIZED;
 
-	if (!lib_initiated())
-		return CKR_CRYPTOKI_NOT_INITIALIZED;
+	if (lib_initiated())
+		rv = ck_encdecrypt_final(hSession, pLastEncryptedPart,
+					 pulLastEncryptedPartLen, CK_FALSE);
 
-	return CKR_FUNCTION_NOT_SUPPORTED;
+	ASSERT_CK_RV(rv, CKR_ARGUMENTS_BAD, CKR_BUFFER_TOO_SMALL,
+		     CKR_CRYPTOKI_NOT_INITIALIZED, CKR_DATA_LEN_RANGE,
+		     CKR_DEVICE_ERROR, CKR_DEVICE_MEMORY, CKR_DEVICE_REMOVED,
+		     CKR_FUNCTION_CANCELED, CKR_FUNCTION_FAILED,
+		     CKR_GENERAL_ERROR, CKR_HOST_MEMORY, CKR_OK,
+		     CKR_OPERATION_NOT_INITIALIZED, CKR_SESSION_CLOSED,
+		     CKR_SESSION_HANDLE_INVALID);
+
+	return rv;
 }
 
 CK_RV C_DecryptInit(CK_SESSION_HANDLE hSession,
 		    CK_MECHANISM_PTR pMechanism,
 		    CK_OBJECT_HANDLE hKey)
 {
-	(void)hSession;
-	(void)pMechanism;
-	(void)hKey;
+	CK_RV rv = CKR_CRYPTOKI_NOT_INITIALIZED;
 
-	if (!lib_initiated())
-		return CKR_CRYPTOKI_NOT_INITIALIZED;
+	if (lib_initiated())
+		rv = ck_encdecrypt_init(hSession, pMechanism, hKey, CK_TRUE);
 
-	return CKR_FUNCTION_NOT_SUPPORTED;
+	ASSERT_CK_RV(rv, CKR_ARGUMENTS_BAD, CKR_CRYPTOKI_NOT_INITIALIZED,
+		     CKR_DEVICE_ERROR, CKR_DEVICE_MEMORY, CKR_DEVICE_REMOVED,
+		     CKR_FUNCTION_CANCELED, CKR_FUNCTION_FAILED,
+		     CKR_GENERAL_ERROR, CKR_HOST_MEMORY,
+		     CKR_KEY_FUNCTION_NOT_PERMITTED, CKR_KEY_HANDLE_INVALID,
+		     CKR_KEY_SIZE_RANGE, CKR_KEY_TYPE_INCONSISTENT,
+		     CKR_MECHANISM_INVALID, CKR_MECHANISM_PARAM_INVALID,
+		     CKR_OK, CKR_OPERATION_ACTIVE, CKR_PIN_EXPIRED,
+		     CKR_SESSION_CLOSED, CKR_SESSION_HANDLE_INVALID,
+		     CKR_USER_NOT_LOGGED_IN);
+
+	return rv;
 }
 
 CK_RV C_Decrypt(CK_SESSION_HANDLE hSession,
@@ -682,16 +719,23 @@ CK_RV C_Decrypt(CK_SESSION_HANDLE hSession,
 		CK_BYTE_PTR pData,
 		CK_ULONG_PTR pulDataLen)
 {
-	(void)hSession;
-	(void)pEncryptedData;
-	(void)ulEncryptedDataLen;
-	(void)pData;
-	(void)pulDataLen;
+	CK_RV rv = CKR_CRYPTOKI_NOT_INITIALIZED;
 
-	if (!lib_initiated())
-		return CKR_CRYPTOKI_NOT_INITIALIZED;
+	if (lib_initiated())
+		rv = ck_encdecrypt_oneshot(hSession, pEncryptedData,
+					   ulEncryptedDataLen,
+					   pData, pulDataLen, CK_TRUE);
 
-	return CKR_FUNCTION_NOT_SUPPORTED;
+	ASSERT_CK_RV(rv, CKR_ARGUMENTS_BAD, CKR_BUFFER_TOO_SMALL,
+		     CKR_CRYPTOKI_NOT_INITIALIZED, CKR_DEVICE_ERROR,
+		     CKR_DEVICE_MEMORY, CKR_DEVICE_REMOVED,
+		     CKR_ENCRYPTED_DATA_INVALID, CKR_ENCRYPTED_DATA_LEN_RANGE,
+		     CKR_FUNCTION_CANCELED, CKR_FUNCTION_FAILED,
+		     CKR_GENERAL_ERROR, CKR_HOST_MEMORY, CKR_OK,
+		     CKR_OPERATION_NOT_INITIALIZED, CKR_SESSION_CLOSED,
+		     CKR_SESSION_HANDLE_INVALID, CKR_USER_NOT_LOGGED_IN);
+
+	return rv;
 }
 
 CK_RV C_DecryptUpdate(CK_SESSION_HANDLE hSession,
@@ -700,30 +744,45 @@ CK_RV C_DecryptUpdate(CK_SESSION_HANDLE hSession,
 		      CK_BYTE_PTR pPart,
 		      CK_ULONG_PTR pulPartLen)
 {
-	(void)hSession;
-	(void)pEncryptedPart;
-	(void)ulEncryptedPartLen;
-	(void)pPart;
-	(void)pulPartLen;
+	CK_RV rv = CKR_CRYPTOKI_NOT_INITIALIZED;
 
-	if (!lib_initiated())
-		return CKR_CRYPTOKI_NOT_INITIALIZED;
+	if (lib_initiated())
+		rv = ck_encdecrypt_update(hSession, pEncryptedPart,
+					  ulEncryptedPartLen,
+					  pPart, pulPartLen, CK_TRUE);
 
-	return CKR_FUNCTION_NOT_SUPPORTED;
+	ASSERT_CK_RV(rv, CKR_ARGUMENTS_BAD, CKR_BUFFER_TOO_SMALL,
+		     CKR_CRYPTOKI_NOT_INITIALIZED, CKR_DEVICE_ERROR,
+		     CKR_DEVICE_MEMORY, CKR_DEVICE_REMOVED,
+		     CKR_ENCRYPTED_DATA_INVALID, CKR_ENCRYPTED_DATA_LEN_RANGE,
+		     CKR_FUNCTION_CANCELED, CKR_FUNCTION_FAILED,
+		     CKR_GENERAL_ERROR, CKR_HOST_MEMORY, CKR_OK,
+		     CKR_OPERATION_NOT_INITIALIZED, CKR_SESSION_CLOSED,
+		     CKR_SESSION_HANDLE_INVALID, CKR_USER_NOT_LOGGED_IN);
+
+	return rv;
 }
 
 CK_RV C_DecryptFinal(CK_SESSION_HANDLE hSession,
 		     CK_BYTE_PTR pLastPart,
 		     CK_ULONG_PTR pulLastPartLen)
 {
-	(void)hSession;
-	(void)pLastPart;
-	(void)pulLastPartLen;
+	CK_RV rv = CKR_CRYPTOKI_NOT_INITIALIZED;
 
-	if (!lib_initiated())
-		return CKR_CRYPTOKI_NOT_INITIALIZED;
+	if (lib_initiated())
+		rv = ck_encdecrypt_final(hSession, pLastPart, pulLastPartLen,
+					 CK_TRUE);
 
-	return CKR_FUNCTION_NOT_SUPPORTED;
+	ASSERT_CK_RV(rv, CKR_ARGUMENTS_BAD, CKR_BUFFER_TOO_SMALL,
+		     CKR_CRYPTOKI_NOT_INITIALIZED, CKR_DEVICE_ERROR,
+		     CKR_DEVICE_MEMORY, CKR_DEVICE_REMOVED,
+		     CKR_ENCRYPTED_DATA_INVALID, CKR_ENCRYPTED_DATA_LEN_RANGE,
+		     CKR_FUNCTION_CANCELED, CKR_FUNCTION_FAILED,
+		     CKR_GENERAL_ERROR, CKR_HOST_MEMORY, CKR_OK,
+		     CKR_OPERATION_NOT_INITIALIZED, CKR_SESSION_CLOSED,
+		     CKR_SESSION_HANDLE_INVALID, CKR_USER_NOT_LOGGED_IN);
+
+	return rv;
 }
 
 CK_RV C_DigestInit(CK_SESSION_HANDLE hSession,

libckteec/src/pkcs11_processing.c
@@ -106,3 +106,239 @@ CK_RV ck_destroy_object(CK_SESSION_HANDLE session, CK_OBJECT_HANDLE obj)
 
 	return rv;
 }
+
+CK_RV ck_encdecrypt_init(CK_SESSION_HANDLE session,
+			 CK_MECHANISM_PTR mechanism,
+			 CK_OBJECT_HANDLE key,
+			 int decrypt)
+{
+	CK_RV rv = CKR_GENERAL_ERROR;
+	TEEC_SharedMemory *ctrl = NULL;
+	struct serializer obj = { };
+	uint32_t session_handle = session;
+	uint32_t key_handle = key;
+	size_t ctrl_size = 0;
+	char *buf = NULL;
+
+	if (!mechanism)
+		return CKR_ARGUMENTS_BAD;
+
+	rv = serialize_ck_mecha_params(&obj, mechanism);
+	if (rv)
+		return rv;
+
+	/*
+	 * Shm io0: (in/out) ctrl
+	 * (in) [session-handle][key-handle][serialized-mechanism-blob]
+	 * (out) [status]
+	 */
+	ctrl_size = sizeof(session_handle) + sizeof(key_handle) + obj.size;
+
+	ctrl = ckteec_alloc_shm(ctrl_size, CKTEEC_SHM_INOUT);
+	if (!ctrl) {
+		rv = CKR_HOST_MEMORY;
+		goto bail;
+	}
+
+	buf = ctrl->buffer;
+
+	memcpy(buf, &session_handle, sizeof(session_handle));
+	buf += sizeof(session_handle);
+
+	memcpy(buf, &key_handle, sizeof(key_handle));
+	buf += sizeof(key_handle);
+
+	memcpy(buf, obj.buffer, obj.size);
+
+	rv = ckteec_invoke_ctrl(decrypt ? PKCS11_CMD_DECRYPT_INIT :
+				PKCS11_CMD_ENCRYPT_INIT, ctrl);
+
+bail:
+	ckteec_free_shm(ctrl);
+	release_serial_object(&obj);
+
+	return rv;
+}
+
+CK_RV ck_encdecrypt_update(CK_SESSION_HANDLE session,
+			   CK_BYTE_PTR in,
+			   CK_ULONG in_len,
+			   CK_BYTE_PTR out,
+			   CK_ULONG_PTR out_len,
+			   int decrypt)
+{
+	CK_RV rv = CKR_GENERAL_ERROR;
+	TEEC_SharedMemory *ctrl = NULL;
+	TEEC_SharedMemory *in_shm = NULL;
+	TEEC_SharedMemory *out_shm = NULL;
+	uint32_t session_handle = session;
+	size_t out_size = 0;
+
+	if ((out_len && *out_len && !out) || (in_len && !in))
+		return CKR_ARGUMENTS_BAD;
+
+	/* Shm io0: (in/out) ctrl = [session-handle] / [status] */
+	ctrl = ckteec_alloc_shm(sizeof(session_handle), CKTEEC_SHM_INOUT);
+	if (!ctrl) {
+		rv = CKR_HOST_MEMORY;
+		goto bail;
+	}
+	memcpy(ctrl->buffer, &session_handle, sizeof(session_handle));
+
+	/* Shm io1: input data buffer if any */
+	if (in_len) {
+		in_shm = ckteec_register_shm(in, in_len, CKTEEC_SHM_IN);
+		if (!in_shm) {
+			rv = CKR_HOST_MEMORY;
+			goto bail;
+		}
+	}
+
+	/* Shm io2: output data buffer */
+	if (out_len && *out_len) {
+		out_shm = ckteec_register_shm(out, *out_len, CKTEEC_SHM_OUT);
+	} else {
+		/* Query output data size */
+		out_shm = ckteec_alloc_shm(0, CKTEEC_SHM_OUT);
+	}
+
+	if (!out_shm) {
+		rv = CKR_HOST_MEMORY;
+		goto bail;
+	}
+
+	/* Invoke */
+	rv = ckteec_invoke_ta(decrypt ? PKCS11_CMD_DECRYPT_UPDATE :
+			      PKCS11_CMD_ENCRYPT_UPDATE, ctrl,
+			      in_shm, out_shm, &out_size, NULL, NULL);
+
+	if (out_len && (rv == CKR_OK || rv == CKR_BUFFER_TOO_SMALL))
+		*out_len = out_size;
+
+	if (rv == CKR_BUFFER_TOO_SMALL && out_size && !out)
+		rv = CKR_OK;
+
+bail:
+	ckteec_free_shm(out_shm);
+	ckteec_free_shm(in_shm);
+	ckteec_free_shm(ctrl);
+
+	return rv;
+}
+
+CK_RV ck_encdecrypt_oneshot(CK_SESSION_HANDLE session,
+			    CK_BYTE_PTR in,
+			    CK_ULONG in_len,
+			    CK_BYTE_PTR out,
+			    CK_ULONG_PTR out_len,
+			    int decrypt)
+{
+	CK_RV rv = CKR_GENERAL_ERROR;
+	TEEC_SharedMemory *ctrl = NULL;
+	TEEC_SharedMemory *in_shm = NULL;
+	TEEC_SharedMemory *out_shm = NULL;
+	uint32_t session_handle = session;
+	size_t out_size = 0;
+
+	if ((out_len && *out_len && !out) || (in_len && !in))
+		return CKR_ARGUMENTS_BAD;
+
+	/* Shm io0: (in/out) ctrl = [session-handle] / [status] */
+	ctrl = ckteec_alloc_shm(sizeof(session_handle), CKTEEC_SHM_INOUT);
+	if (!ctrl) {
+		rv = CKR_HOST_MEMORY;
+		goto bail;
+	}
+	memcpy(ctrl->buffer, &session_handle, sizeof(session_handle));
+
+	/* Shm io1: input data buffer */
+	if (in_len) {
+		in_shm = ckteec_register_shm(in, in_len, CKTEEC_SHM_IN);
+		if (!in_shm) {
+			rv = CKR_HOST_MEMORY;
+			goto bail;
+		}
+	}
+
+	/* Shm io2: output data buffer */
+	if (out_len && *out_len) {
+		out_shm = ckteec_register_shm(out, *out_len, CKTEEC_SHM_OUT);
+	} else {
+		/* Query output data size */
+		out_shm = ckteec_alloc_shm(0, CKTEEC_SHM_OUT);
+	}
+
+	if (!out_shm) {
+		rv = CKR_HOST_MEMORY;
+		goto bail;
+	}
+
+	rv = ckteec_invoke_ta(decrypt ? PKCS11_CMD_DECRYPT_ONESHOT :
+			      PKCS11_CMD_ENCRYPT_ONESHOT, ctrl,
+			      in_shm, out_shm, &out_size, NULL, NULL);
+
+	if (out_len && (rv == CKR_OK || rv == CKR_BUFFER_TOO_SMALL))
+		*out_len = out_size;
+
+	if (rv == CKR_BUFFER_TOO_SMALL && out_size && !out)
+		rv = CKR_OK;
+
+bail:
+	ckteec_free_shm(out_shm);
+	ckteec_free_shm(in_shm);
+	ckteec_free_shm(ctrl);
+
+	return rv;
+}
+
+CK_RV ck_encdecrypt_final(CK_SESSION_HANDLE session,
+			  CK_BYTE_PTR out,
+			  CK_ULONG_PTR out_len,
+			  int decrypt)
+{
+	CK_RV rv = CKR_GENERAL_ERROR;
+	TEEC_SharedMemory *ctrl = NULL;
+	TEEC_SharedMemory *out_shm = NULL;
+	uint32_t session_handle = session;
+	size_t out_size = 0;
+
+	if (out_len && *out_len && !out)
+		return CKR_ARGUMENTS_BAD;
+
+	/* Shm io0: (in/out) ctrl = [session-handle] / [status] */
+	ctrl = ckteec_alloc_shm(sizeof(session_handle), CKTEEC_SHM_INOUT);
+	if (!ctrl) {
+		rv = CKR_HOST_MEMORY;
+		goto bail;
+	}
+	memcpy(ctrl->buffer, &session_handle, sizeof(session_handle));
+
+	/* Shm io2: output buffer reference */
+	if (out_len && *out_len) {
+		out_shm = ckteec_register_shm(out, *out_len, CKTEEC_SHM_OUT);
+	} else {
+		/* Query output data size */
+		out_shm = ckteec_alloc_shm(0, CKTEEC_SHM_OUT);
+	}
+
+	if (!out_shm) {
+		rv = CKR_HOST_MEMORY;
+		goto bail;
+	}
+
+	rv = ckteec_invoke_ctrl_out(decrypt ? PKCS11_CMD_DECRYPT_FINAL :
+				    PKCS11_CMD_ENCRYPT_FINAL,
+				    ctrl, out_shm, &out_size);
+
+	if (out_len && (rv == CKR_OK || rv == CKR_BUFFER_TOO_SMALL))
+		*out_len = out_size;
+
+	if (rv == CKR_BUFFER_TOO_SMALL && out_size && !out)
+		rv = CKR_OK;
+
+bail:
+	ckteec_free_shm(out_shm);
+	ckteec_free_shm(ctrl);
+
+	return rv;
+}

libckteec/src/pkcs11_processing.h
@@ -12,4 +12,28 @@ CK_RV ck_create_object(CK_SESSION_HANDLE session, CK_ATTRIBUTE_PTR attribs,
 		       CK_ULONG count, CK_OBJECT_HANDLE_PTR phObject);
 
 CK_RV ck_destroy_object(CK_SESSION_HANDLE session, CK_OBJECT_HANDLE obj);
+
+CK_RV ck_encdecrypt_init(CK_SESSION_HANDLE session,
+			 CK_MECHANISM_PTR mechanism,
+			 CK_OBJECT_HANDLE key,
+			 int decrypt);
+
+CK_RV ck_encdecrypt_update(CK_SESSION_HANDLE session,
+			   CK_BYTE_PTR in,
+			   CK_ULONG in_len,
+			   CK_BYTE_PTR out,
+			   CK_ULONG_PTR out_len,
+			   int decrypt);
+
+CK_RV ck_encdecrypt_oneshot(CK_SESSION_HANDLE session,
+			    CK_BYTE_PTR in,
+			    CK_ULONG in_len,
+			    CK_BYTE_PTR out,
+			    CK_ULONG_PTR out_len,
+			    int decrypt);
+
+CK_RV ck_encdecrypt_final(CK_SESSION_HANDLE session,
+			  CK_BYTE_PTR out,
+			  CK_ULONG_PTR out_len,
+			  int decrypt);
 #endif /*LIBCKTEEC_PKCS11_PROCESSING_H*/

libckteec/src/serialize_ck.h
@@ -13,4 +13,7 @@
 CK_RV serialize_ck_attributes(struct serializer *obj,
 			      CK_ATTRIBUTE_PTR attributes, CK_ULONG count);
 
+/* Create (and allocate) a serial object for CK_MECHANISM array */
+CK_RV serialize_ck_mecha_params(struct serializer *obj,
+				CK_MECHANISM_PTR mechanisms);
 #endif /*LIBCKTEEC_SERIALIZE_CK_H*/

libckteec/src/serializer.c
@@ -67,3 +67,10 @@ CK_RV serialize_32b(struct serializer *obj, uint32_t data)
 {
 	return serialize_buffer(obj, &data, sizeof(data));
 }
+
+CK_RV serialize_ck_ulong(struct serializer *obj, CK_ULONG data)
+{
+	uint32_t data32 = data;
+
+	return serialize_buffer(obj, &data32, sizeof(data32));
+}

libckteec/src/serializer.h
@@ -18,6 +18,8 @@ struct serializer {
 	char *buffer;		/* serial buffer base address */
 	size_t size;		/* serial buffer current byte size */
 	size_t item_count;	/* number of items in entry table */
+	uint32_t object;
+	uint32_t type;
 };
 
 /* Init/finalize/release a serializer object */
@@ -27,5 +29,6 @@ void release_serial_object(struct serializer *obj);
 
 CK_RV serialize_buffer(struct serializer *obj, void *data, size_t size);
 CK_RV serialize_32b(struct serializer *obj, uint32_t data);
+CK_RV serialize_ck_ulong(struct serializer *obj, CK_ULONG data);
 
 #endif /*LIBCKTEEC_SERIALIZER_H*/

commit 29e7073c554b9448916c1d01409db921322df118

libckteec: fix NULL pointer dereferences in PKCS#11 serialization
Multiple functions in serialize_ck.c dereference mechanism parameters
and attribute values without NULL checks, causing crashes when
processing malformed input:

- serialize_mecha_aes_ctr / aes_gcm / aes_cbc_encrypt_data /
  aes_iv / key_deriv_str / ecdh1_derive_param / rsa_pss_param /
  rsa_oaep_param / rsa_aes_key_wrap / eddsa /
  mac_general_param: NULL-check pParameter (and, in the OAEP/ECDH1
  cases, defer the size computation until after the check).
- serialize_mecha_rsa_aes_key_wrap also NULL-checks
  params->pOAEPParams, which was previously dereferenced
  unconditionally on the variable initialiser line.

All handlers return CKR_MECHANISM_PARAM_INVALID for NULL
pParameter, matching the existing return code used elsewhere in the
file.

CWE-476: NULL Pointer Dereference
Found by fuzzing with AFL++/AddressSanitizer.

Fixes: e7a4706e08c8 ("libckteec: add support for symmetric ciphers")
Fixes: 0698b4264ec2 ("libckteec: serialize_ck: Add helper functions for mechanisms")
Fixes: 98dc4f488c2f ("libckteec: serialize_ck.c: serialize AES-GCM mechanism parameters")
Fixes: 8b3f7fe3401f ("libckteec: add support for ECDH derive")
Fixes: 6e94ff57e0f7 ("libckteec: Add PKCS #1 RSA PSS signing support")
Fixes: cd66d4071ca8 ("libckteec: Add PKCS #1 RSA OAEP encryption support")
Fixes: d26d885c82d2 ("libckteec: Add RSA AES key wrap serialization")
Fixes: 719780674ee9 ("libckteec: Add support for more HMAC mechanisms")
Fixes: 140bf4630460 ("libckteec: Add EDDSA attribute serialization")
Reported-by: Tobi Gaertner <tggaertn@amazon.com>
Signed-off-by: Georges Savoundararadj <savoundg@amazon.com>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

libckteec/src/serialize_ck.c
@@ -393,6 +393,9 @@ static CK_RV serialize_mecha_aes_ctr(struct serializer *obj,
 	CK_RV rv = CKR_GENERAL_ERROR;
 	uint32_t size = 0;
 
+	if (!param)
+		return CKR_MECHANISM_PARAM_INVALID;
+
 	rv = serialize_32b(obj, obj->type);
 	if (rv)
 		return rv;
@@ -419,6 +422,9 @@ static CK_RV serialize_mecha_aes_gcm(struct serializer *obj,
 	CK_GCM_PARAMS_PTR param = mecha->pParameter;
 	CK_RV rv = CKR_GENERAL_ERROR;
 	CK_ULONG aad_len = 0;
+
+	if (!param)
+		return CKR_MECHANISM_PARAM_INVALID;
 
 	/* AAD is not manadatory */
 	if (param->pAAD)
@@ -461,6 +467,9 @@ static CK_RV serialize_mecha_aes_iv(struct serializer *obj,
 	uint32_t iv_size = mecha->ulParameterLen;
 	CK_RV rv = CKR_GENERAL_ERROR;
 
+	if (iv_size && !mecha->pParameter)
+		return CKR_MECHANISM_PARAM_INVALID;
+
 	rv = serialize_32b(obj, obj->type);
 	if (rv)
 		return rv;
@@ -478,6 +487,9 @@ static CK_RV serialize_mecha_key_deriv_str(struct serializer *obj,
 	CK_KEY_DERIVATION_STRING_DATA_PTR param = mecha->pParameter;
 	CK_RV rv = CKR_GENERAL_ERROR;
 	uint32_t size = 0;
+
+	if (!param)
+		return CKR_MECHANISM_PARAM_INVALID;
 
 	rv = serialize_32b(obj, obj->type);
 	if (rv)
@@ -500,8 +512,13 @@ static CK_RV serialize_mecha_ecdh1_derive_param(struct serializer *obj,
 {
 	CK_ECDH1_DERIVE_PARAMS *params = mecha->pParameter;
 	CK_RV rv = CKR_GENERAL_ERROR;
-	size_t params_size = 3 * sizeof(uint32_t) + params->ulSharedDataLen +
-			     params->ulPublicDataLen;
+	size_t params_size = 0;
+
+	if (!params)
+		return CKR_MECHANISM_PARAM_INVALID;
+
+	params_size = 3 * sizeof(uint32_t) + params->ulSharedDataLen +
+		      params->ulPublicDataLen;
 
 	rv = serialize_32b(obj, obj->type);
 	if (rv)
@@ -539,6 +556,9 @@ static CK_RV serialize_mecha_aes_cbc_encrypt_data(struct serializer *obj,
 	CK_RV rv = CKR_GENERAL_ERROR;
 	uint32_t size = 0;
 
+	if (!param)
+		return CKR_MECHANISM_PARAM_INVALID;
+
 	rv = serialize_32b(obj, obj->type);
 	if (rv)
 		return rv;
@@ -565,6 +585,9 @@ static CK_RV serialize_mecha_rsa_pss_param(struct serializer *obj,
 	CK_RSA_PKCS_PSS_PARAMS *params = mecha->pParameter;
 	CK_RV rv = CKR_GENERAL_ERROR;
 	uint32_t params_size = 3 * sizeof(uint32_t);
+
+	if (!params)
+		return CKR_MECHANISM_PARAM_INVALID;
 
 	if (mecha->ulParameterLen != sizeof(*params))
 		return CKR_ARGUMENTS_BAD;
@@ -593,10 +616,15 @@ static CK_RV serialize_mecha_rsa_oaep_param(struct serializer *obj,
 {
 	CK_RSA_PKCS_OAEP_PARAMS *params = mecha->pParameter;
 	CK_RV rv = CKR_GENERAL_ERROR;
-	size_t params_size = 4 * sizeof(uint32_t) + params->ulSourceDataLen;
+	size_t params_size = 0;
+
+	if (!params)
+		return CKR_MECHANISM_PARAM_INVALID;
 
 	if (mecha->ulParameterLen != sizeof(*params))
 		return CKR_ARGUMENTS_BAD;
+
+	params_size = 4 * sizeof(uint32_t) + params->ulSourceDataLen;
 
 	rv = serialize_32b(obj, obj->type);
 	if (rv)
@@ -630,12 +658,18 @@ static CK_RV serialize_mecha_rsa_aes_key_wrap(struct serializer *obj,
 					      CK_MECHANISM_PTR mecha)
 {
 	CK_RSA_AES_KEY_WRAP_PARAMS *params = mecha->pParameter;
-	CK_RSA_PKCS_OAEP_PARAMS *aes_params = params->pOAEPParams;
+	CK_RSA_PKCS_OAEP_PARAMS *aes_params = NULL;
 	CK_RV rv = CKR_GENERAL_ERROR;
-	size_t params_size = 5 * sizeof(uint32_t) + aes_params->ulSourceDataLen;
+	size_t params_size = 0;
+
+	if (!params || !params->pOAEPParams)
+		return CKR_MECHANISM_PARAM_INVALID;
 
 	if (mecha->ulParameterLen != sizeof(*params))
 		return CKR_ARGUMENTS_BAD;
+
+	aes_params = params->pOAEPParams;
+	params_size = 5 * sizeof(uint32_t) + aes_params->ulSourceDataLen;
 
 	rv = serialize_32b(obj, obj->type);
 	if (rv)
@@ -689,6 +723,9 @@ static CK_RV serialize_mecha_eddsa(struct serializer *obj,
 		params_len = sizeof(*params);
 	}
 
+	if (!params)
+		return CKR_MECHANISM_PARAM_INVALID;
+
 	if (params_len != sizeof(*params))
 		return CKR_ARGUMENTS_BAD;
 
@@ -716,6 +753,9 @@ static CK_RV serialize_mecha_mac_general_param(struct serializer *obj,
 {
 	CK_RV rv = CKR_GENERAL_ERROR;
 	CK_ULONG ck_data = 0;
+
+	if (!mecha->pParameter)
+		return CKR_MECHANISM_PARAM_INVALID;
 
 	if (mecha->ulParameterLen != sizeof(ck_data))
 		return CKR_ARGUMENTS_BAD;